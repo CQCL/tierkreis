9f4d3e198df94d2b786b908fb5254c4fbc51ef0c
seyon <seyon.sivarajah@cambridgequantum.com>
2021-06-18 16:16:35 +0100

    add more graph methods

--- core/tierkreis/core/tierkreis_graph.py
+++ core/tierkreis/core/tierkreis_graph.py
@@ -2,7 +2,18 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import Any, Dict, Tuple, cast
+from typing import (
+    Any,
+    Dict,
+    Iterator,
+    List,
+    Optional,
+    Tuple,
+    Type,
+    Union,
+    cast,
+    NewType,
+)
 
 import networkx as nx  # type: ignore
 import tierkreis.core.protos.tierkreis.graph as pg
@@ -10,9 +21,10 @@ from tierkreis.core.types import TierkreisType
 from tierkreis.core.values import TierkreisValue
 import betterproto
 
-# import graphviz as gv  # type: ignore
+import graphviz as gv  # type: ignore
 
-FunctionID = str
+FunctionID = NewType("FunctionID", str)
+PortID = NewType("PortID", str)
 
 SIGNATURE: Dict[
     FunctionID, Tuple[Dict[str, TierkreisType], Dict[str, TierkreisType]]
@@ -28,18 +40,6 @@ class TierkreisNode(ABC):
     @abstractmethod
     def outputs(self) -> Dict[str, TierkreisType]:
         pass
-
-    @property
-    def in_port(self) -> Enum:
-        return Enum(
-            type(self).__name__ + ".in_port", [(n, n) for n in self.inputs().keys()]
-        )
-
-    @property
-    def out_port(self) -> Enum:
-        return Enum(
-            type(self).__name__ + ".out_port", [(n, n) for n in self.outputs().keys()]
-        )
 
     @abstractmethod
     def to_proto(self) -> pg.Node:
@@ -64,21 +64,6 @@ class TierkreisNode(ABC):
 
 
 @dataclass
-class ConstNode(TierkreisNode):
-    value: TierkreisValue
-
-    def inputs(self) -> Dict[str, TierkreisType]:
-        return dict()
-
-    def outputs(self) -> Dict[str, TierkreisType]:
-        # TODO return "unknown type"
-        return NotImplemented
-
-    def to_proto(self) -> pg.Node:
-        return pg.Node(const=self.value.to_proto())
-
-
-@dataclass
 class InputNode(TierkreisNode):
     _inputs: Dict[str, TierkreisType] = field(default_factory=dict)
 
@@ -90,6 +75,9 @@ class InputNode(TierkreisNode):
 
     def to_proto(self) -> pg.Node:
         return pg.Node(input=pg.Empty())
+
+    def set_input(self, name: str, edge_type: TierkreisType) -> None:
+        self._inputs[name] = edge_type
 
 
 @dataclass
@@ -104,6 +92,24 @@ class OutputNode(TierkreisNode):
 
     def to_proto(self) -> pg.Node:
         return pg.Node(output=pg.Empty())
+
+    def set_output(self, name: str, edge_type: TierkreisType) -> None:
+        self._outputs[name] = edge_type
+
+
+@dataclass
+class ConstNode(TierkreisNode):
+    value: TierkreisValue
+
+    def inputs(self) -> Dict[str, TierkreisType]:
+        return dict()
+
+    def outputs(self) -> Dict[str, TierkreisType]:
+        # TODO return "unknown type"
+        return {"value": NotImplemented}
+
+    def to_proto(self) -> pg.Node:
+        return pg.Node(const=self.value.to_proto())
 
 
 @dataclass
@@ -134,135 +140,189 @@ class FunctionNode(TierkreisNode):
         return pg.Node(function=self.function)
 
 
-# @dataclass
-# class TierkreisEdge:
-#     source: NodePort
-#     target: NodePort
-#     type: Optional[TierkreisType]
+@dataclass(frozen=True)
+class NodeRef:
+    name: str
+    node: TierkreisNode
 
-#     def to_proto(self) -> pg.Edge:
-#         if self.type is None:
-#             edge_type = None
-#         else:
-#             edge_type = self.type.to_proto()
+    @dataclass
+    class Ports:
+        class PortNotFound(Exception):
+            pass
 
-#         return pg.Edge(
-#             port_from=self.source.port,
-#             node_from=self.source.node,
-#             port_to=self.target.port,
-#             node_to=self.target.node,
-#             edge_type=cast(pg.Type, edge_type),
-#         )
+        _node_ref: "NodeRef"
+        _ports: Dict[str, TierkreisType]
 
-#     @staticmethod
-#     def from_proto(edge: pg.Edge) -> GraphEdge:
-#         return GraphEdge(
-#             source=NodePort(
-#                 node=edge.node_from,
-#                 port=edge.port_from,
-#             ),
-#             target=NodePort(node=edge.node_to, port=edge.port_to),
-#             type=TierkreisType.from_proto(edge.edge_type),
-#         )
+        def __getattribute__(self, name: str) -> "NodePort":
+            if name in super().__getattribute__("_ports"):
+                return NodePort(super().__getattribute__("_node_ref"), PortID(name))
+            raise super().__getattribute__("PortNotFound")(name)
+
+    @property
+    def in_port(self) -> "NodeRef.Ports":
+        return self.Ports(self, self.node.inputs())
+
+    @property
+    def out_port(self) -> "NodeRef.Ports":
+        return self.Ports(self, self.node.outputs())
+
+
+@dataclass(frozen=True)
+class NodePort:
+    node_ref: NodeRef
+    port: PortID
+
+
+@dataclass
+class TierkreisEdge:
+    source: NodePort
+    target: NodePort
+    type: Optional[TierkreisType]
+
+    def to_proto(self) -> pg.Edge:
+        edge_type = None if self.type is None else self.type.to_proto()
+
+        return pg.Edge(
+            port_from=self.source.port,
+            node_from=self.source.node_ref.name,
+            port_to=self.target.port,
+            node_to=self.target.node_ref.name,
+            edge_type=cast(pg.Type, edge_type),
+        )
 
 
 class TierkreisGraph:
     """Builder for tierkreis graphs."""
 
-    def __init__(self) -> None:
+    input_node_name: str = "input"
+    output_node_name: str = "output"
+
+    def __init__(self, name: Optional[str] = None) -> None:
+        self.name = name
         self._graph = nx.MultiDiGraph()
         self._graph.add_node(self.input_node_name, node_info=InputNode())
         self._graph.add_node(self.output_node_name, node_info=OutputNode())
 
-        self._input_types: Dict[str, TierkreisType] = dict()
-        self._output_types: Dict[str, TierkreisType] = dict()
+        # self._name_counts = {name: 0 for name in ("const, box, function")}
 
     @property
     def n_nodes(self) -> int:
         return len(self._graph)
 
     @property
-    def input_node_name(self) -> str:
-        return "input"
-
-    @property
-    def output_node_name(self) -> str:
-        return "output"
-
-    @property
     def inputs(self) -> Dict[str, TierkreisType]:
-        return self._input_types
+        return self[self.input_node_name].node.outputs()
 
     @property
     def outputs(self) -> Dict[str, TierkreisType]:
-        return self._output_types
+        return self[self.output_node_name].node.inputs()
 
-    def add_node(self, name: str, node: TierkreisNode):
+    def _add_node(self, name: str, node: TierkreisNode) -> NodeRef:
         self._graph.add_node(name, node_info=node)
-        return name
+        return NodeRef(name, node)
 
     def add_function_node(
         self,
         name: str,
         function: str,
-    ) -> str:
-        return self.add_node(name, FunctionNode(function))
+    ) -> NodeRef:
+        return self._add_node(name, FunctionNode(FunctionID(function)))
 
-    def add_const(self, name: str, value: Any) -> str:
+    def add_const(self, name: str, value: Any) -> NodeRef:
         tkval = (
             value
             if isinstance(value, TierkreisValue)
             else TierkreisValue.from_python(value)
         )
-        return self.add_node(name, ConstNode(tkval))
+        return self._add_node(name, ConstNode(tkval))
 
-    def add_box(self, name: str, graph: "TierkreisGraph") -> str:
-        return self.add_node(name, BoxNode(graph))
+    def add_box(self, name: str, graph: "TierkreisGraph") -> NodeRef:
+        return self._add_node(name, BoxNode(graph))
 
-    @property
     def nodes(self) -> Dict[str, TierkreisNode]:
-        # TODO make a dictionary view instead
         return {
             name: self._graph.nodes[name]["node_info"] for name in self._graph.nodes
         }
 
-    # def add_edge(
-    #     self,
-    #     node_port_from: Tuple[str, str],
-    #     node_port_to: Tuple[str, str],
-    #     edge_type: Optional[Type] = None,
-    # ):
-    #     new_edge = pg.Edge()
-    #     new_edge.node_from, new_edge.port_from = node_port_from
-    #     new_edge.node_to, new_edge.port_to = node_port_to
-    #     self._g.edges.append(new_edge)
-    #     if edge_type is not None:
-    #         new_edge.edge_type = from_python_type(edge_type)
+    def edges(self) -> List[TierkreisEdge]:
+        return [
+            cast(TierkreisEdge, edge_info[2])
+            for edge_info in self._graph.edges(data="edge_info", keys=False)
+        ]
 
-    #     if node_port_from[0] == "input":
-    #         self._input_types[node_port_from[1]] = edge_type
-    #     if node_port_to[0] == "output":
-    #         self._output_types[node_port_to[1]] = edge_type
+    def __getitem__(self, key: Union[str, NodeRef]) -> NodeRef:
+        if isinstance(key, NodeRef):
+            if not self._graph.has_node(key.name):
+                raise KeyError(f"Graph does not contain node with name {key.name}")
+            return key
+        return NodeRef(key, self._graph.nodes[key]["node_info"])
 
-    # def register_input(self, name: str, edge_type: Type, node_port: Tuple[str, str]):
-    #     self._input_types[name] = edge_type
-    #     self.add_edge((self.input_node, name), node_port, edge_type)
+    def add_edge(
+        self,
+        node_port_from: NodePort,
+        node_port_to: NodePort,
+        edge_type: Optional[Union[Type, TierkreisType]] = None,
+    ) -> TierkreisEdge:
+        if edge_type is None:
+            tk_type = None
+        else:
+            tk_type = (
+                edge_type
+                if isinstance(edge_type, TierkreisType)
+                else TierkreisType.from_python(edge_type)
+            )
 
-    # def register_output(self, name: str, edge_type: Type, node_port: Tuple[str, str]):
-    #     self._output_types[name] = edge_type
-    #     self.add_edge(node_port, (self.output_node, name), edge_type)
+        edge = TierkreisEdge(node_port_from, node_port_to, tk_type)
 
-    # def incoming_edges(self, node: str) -> List[pg.Edge]:
-    #     return [e for e in self._g.edges if e.node_to == node]
+        self._graph.add_edge(
+            node_port_from.node_ref.name,
+            node_port_to.node_ref.name,
+            edge_info=edge,
+        )
+        return edge
 
-    # def outgoing_edges(self, node: str) -> List[pg.Edge]:
-    #     return [e for e in self._g.edges if e.node_from == node]
+    def register_input(
+        self, name: str, node_port: NodePort, edge_type: Optional[Type] = None
+    ):
+        node = cast(InputNode, self[self.input_node_name].node)
+        if edge_type is None:
+            tk_type = node_port.node_ref.node.inputs()[node_port.port]
+            # TODO check if tk_type is valid or unknown (e.g. ConstNode)
+        else:
+            tk_type = edge_type
+        node.set_input(name, tk_type)
 
-    # def input_edges(self) -> List[pg.Edge]:
-    #     return self.outgoing_edges(self.input_node)
+        self.add_edge(NodePort(self[self.input_node_name], PortID(name)), node_port)
 
-    # def output_edges(self) -> List[pg.Edge]:
-    #     return self.incoming_edges(self.output_node)
+    def register_output(
+        self, name: str, node_port: NodePort, edge_type: Optional[Type] = None
+    ):
+        node = cast(OutputNode, self[self.output_node_name].node)
+        if edge_type is None:
+            tk_type = node_port.node_ref.node.outputs()[node_port.port]
+            # TODO check if tk_type is valid or unknown
+        else:
+            tk_type = edge_type
+        node.set_output(name, tk_type)
+        self.add_edge(node_port, NodePort(self[self.output_node_name], PortID(name)))
+
+    def in_edges(self, node: Union[NodeRef, str]) -> List[TierkreisEdge]:
+        node_name = node if isinstance(node, str) else node.name
+        return [
+            cast(TierkreisEdge, edge_info[2])
+            for edge_info in self._graph.in_edges(
+                node_name, data="edge_info", keys=False
+            )
+        ]
+
+    def out_edges(self, node: Union[NodeRef, str]) -> List[TierkreisEdge]:
+        node_name = node if isinstance(node, str) else node.name
+        return [
+            cast(TierkreisEdge, edge_info[2])
+            for edge_info in self._graph.out_edges(
+                node_name, data="edge_info", keys=False
+            )
+        ]
 
     # def remove_edge(
     #     self, node_port_from: Tuple[str, str], node_port_to: Tuple[str, str]
@@ -279,25 +339,147 @@ class TierkreisGraph:
     #     self._g.edges.remove(edge)
     #     return edge
 
-    # def get_type(self) -> Type:
-    #     return type(
-    #         "ProtoGraphBuilder",
-    #         (ProtoGraphBuilder,),
-    #         {"inputs": self._input_types, "outputs": self._output_types},
-    #     )
-
-    # def write_to_file(self, file_pointer: IO[bytes]):
-    #     file_pointer.write(self._g.SerializeToString())
-
-    # @classmethod
-    # def read_from_file(cls, file_pointer: IO[bytes]) -> "ProtoGraphBuilder":
-    #     new = cls()
-    #     new._g.parse(file_pointer.read())
-    #     return new
-
     def to_proto(self) -> pg.Graph:
-        return NotImplemented
+        pg_graph = pg.Graph()
+        pg_graph.nodes = {
+            node_name: self[node_name].node.to_proto()
+            for node_name in cast(Iterator[str], self._graph.nodes)
+        }
+        pg_graph.edges = [
+            cast(TierkreisEdge, edge_info[2]).to_proto()
+            for edge_info in self._graph.edges(data="edge_info", keys=False)
+        ]
+        return pg_graph
 
     @staticmethod
-    def from_proto(graph: pg.Graph) -> "TierkreisGraph":
-        return NotImplemented
+    def from_proto(pg_graph: pg.Graph) -> "TierkreisGraph":
+        tk_graph = TierkreisGraph()
+        for node_name, pg_node in pg_graph.nodes.items():
+            tk_graph._add_node(node_name, TierkreisNode.from_proto(pg_node))
+        for pg_edge in pg_graph.edges:
+            source_node = tk_graph[pg_edge.node_from]
+            target_node = tk_graph[pg_edge.node_to]
+            source = NodePort(source_node, PortID(pg_edge.port_from))
+            target = NodePort(target_node, PortID(pg_edge.port_to))
+            tk_graph.add_edge(
+                source, target, TierkreisType.from_proto(pg_edge.edge_type)
+            )
+        return tk_graph
+
+
+def tierkreis_graphviz(tk_graph: TierkreisGraph) -> gv.Digraph:
+    """
+    Return a visual representation of the DAG as a graphviz object.
+
+    :returns:   Representation of the DAG
+    :rtype:     graphviz.DiGraph
+    """
+    gv_graph = gv.Digraph(
+        "TierKreis",
+        strict=True,
+    )
+
+    gv_graph.attr(rankdir="LR", ranksep="0.3", nodesep="0.15", margin="0")
+    wire_color = "red"
+    task_color = "darkolivegreen3"
+    io_color = "green"
+    out_color = "black"
+    in_color = "white"
+
+    boundary_node_attr = {"fontname": "Courier", "fontsize": "8"}
+    boundary_nodes = {tk_graph.input_node_name, tk_graph.output_node_name}
+
+    with gv_graph.subgraph(name="cluster_input") as c:
+        c.attr(rank="source")
+        c.node_attr.update(shape="point", color=io_color)
+        for port in tk_graph.inputs:
+            c.node(
+                name=f"({tk_graph.input_node_name}out, {port})",
+                xlabel="Input" + str(port),
+                **boundary_node_attr,
+            )
+
+    with gv_graph.subgraph(name="cluster_output") as c:
+        c.attr(rank="sink")
+        c.node_attr.update(shape="point", color=io_color)
+        for port in tk_graph.outputs:
+            c.node(
+                name=f"({tk_graph.output_node_name}in, {port})",
+                # name=str(((str(self._o) + "in").replace("::", "_"), i)),
+                xlabel="Output",
+                **boundary_node_attr,
+            )
+
+    node_cluster_attr = {
+        "style": "rounded, filled",
+        "color": task_color,
+        "fontname": "Times-Roman",
+        "fontsize": "10",
+        "margin": "5",
+        "lheight": "100",
+    }
+    in_port_node_attr = {
+        "color": in_color,
+        "shape": "point",
+        "weight": "2",
+        "fontname": "Helvetica",
+        "fontsize": "8",
+        "rank": "source",
+    }
+    out_port_node_attr = {
+        "color": out_color,
+        "shape": "point",
+        "weight": "2",
+        "fontname": "Helvetica",
+        "fontsize": "8",
+        "rank": "sink",
+    }
+    count = 0
+
+    for node_name in tk_graph.nodes():
+
+        if node_name not in boundary_nodes:
+            with gv_graph.subgraph(name=f"cluster_{node_name}{count}") as c:
+                count = count + 1
+                c.attr(label=node_name, **node_cluster_attr)
+
+                incoming_edges = tk_graph.in_edges(node_name)
+
+                for e in incoming_edges:
+                    c.node(
+                        name=f"({node_name}in, {e.target.port})",
+                        # name=str(((str(node) + "in").replace("::", "-"), i)),
+                        xlabel=str(e.target.port),
+                        **in_port_node_attr,
+                    )
+
+                outgoing_edges = tk_graph.out_edges(node_name)
+                # if len(outgoing_edges) == 1:
+                #     c.node(
+                #         name=f"({node_name}out, 0)",
+                #         **out_port_node_attr,
+                #     )
+                # else:
+                for e in outgoing_edges:
+                    c.node(
+                        name=f"({node_name}out, {e.source.port})",
+                        xlabel=str(e.source.port),
+                        **out_port_node_attr,
+                    )
+
+    edge_attr = {
+        "weight": "2",
+        "arrowhead": "vee",
+        "arrowsize": "0.2",
+        "headclip": "true",
+        "tailclip": "true",
+    }
+    print(list(gv_graph))
+    for edge in tk_graph.edges():
+        src_nodename = f"({edge.source.node_ref.name}out, {edge.source.port})"
+        tgt_nodename = f"({edge.target.node_ref.name}in, {edge.target.port})"
+        print("edge", src_nodename, tgt_nodename)
+
+        gv_graph.edge(src_nodename, tgt_nodename, color=wire_color, **edge_attr)
+
+    return gv_graph

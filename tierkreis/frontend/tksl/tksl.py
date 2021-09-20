735720dffdec6e538fc1f3022432756b68a0fb7f
seyon <seyon.sivarajah@cambridgequantum.com>
2021-09-20 15:43:08 +0100

    add if, loop, struct type

--- tierkreis/frontend/tksl/tksl.py
+++ tierkreis/frontend/tksl/tksl.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+from dataclasses import dataclass, field, make_dataclass
 from pathlib import Path
+import copy
 import sys
-from typing import Any, Iterator, Dict, List, Optional, Set, Tuple
+from typing import Any, Iterable, Iterator, Dict, List, Optional, Set, Tuple, cast
 from lark import Lark
+from lark.lexer import Token
+from lark.tree import Tree
+from networkx import convert
 from tierkreis import TierkreisGraph
 from tierkreis.core.function import TierkreisFunction
 from tierkreis.core.tierkreis_graph import NodePort, NodeRef
@@ -14,15 +19,34 @@ from tierkreis.core.types import (
     PairType,
     Row,
     StringType,
+    StructType,
     TierkreisType,
     TypeScheme,
     ArrayType,
     VarType,
 )
+from tierkreis.core.tierkreis_struct import TierkreisStruct
 from tierkreis.frontend import local_runtime, RuntimeClient
 from tierkreis.core.graphviz import tierkreis_to_graphviz
 
 FuncDefs = Dict[str, Tuple[TierkreisGraph, TierkreisFunction]]
+
+
+@dataclass
+class Context:
+    functions: Dict[str, Tuple[TierkreisGraph, TierkreisFunction]] = field(
+        default_factory=dict
+    )
+    output_vars: Dict[str, Tuple[NodeRef, TierkreisFunction]] = field(
+        default_factory=dict
+    )
+    constants: Dict[str, NodeRef] = field(default_factory=dict)
+
+    inputs: Dict[str, TierkreisType] = field(default_factory=dict)
+    outputs: Dict[str, TierkreisType] = field(default_factory=dict)
+
+    def copy(self) -> "Context":
+        return copy.deepcopy(self)
 
 
 def get_client() -> Iterator[RuntimeClient]:
@@ -52,6 +76,19 @@ def get_const(token) -> Any:
         return token.children[0].value == "True"
     if token.data == "str":
         return str(token.children[0].value[1:-1])
+    if token.data == "struct":
+        struct_id = token.children[0]
+        if struct_id.data == "anon":
+            pass
+        else:
+            raise RuntimeError # TODO aliases
+        
+        field_names = [const_assign.children[0].value for const_assign in token.children[1:]]
+        values = [get_const(const_assign.children[1]) for const_assign in token.children[1:]]
+        print(values)
+        cl = make_dataclass("anon_struct", fields=field_names, bases=(TierkreisStruct,))
+
+        return cl(**dict(zip(field_names, values)))
 
 
 def get_type(token) -> TierkreisType:
@@ -68,6 +105,9 @@ def get_type(token) -> TierkreisType:
         return PairType(get_type(token.children[1]), get_type(token.children[2]))
     if type_name == "TYPE_ARRAY":
         return ArrayType(get_type(token.children[1]))
+    if type_name == "TYPE_STRUCT":
+        args = token.children[1].children
+        return StructType(Row({arg.children[0].value: get_type(arg.children[1]) for arg in args}))
     return VarType("unkown")
 
 
@@ -82,53 +122,79 @@ def get_inp_out(f_tree) -> Tuple[Dict[str, TierkreisType], Dict[str, TierkreisTy
     return get_annotations(f_tree.children[1]), get_annotations(f_tree.children[2])
 
 
-def get_tkfunc_def(f_tree) -> TierkreisFunction:
-    inputs, outputs = get_inp_out(f_tree)
+def gen_tkfunc(
+    name: str, inputs: Dict[str, TierkreisType], outputs: Dict[str, TierkreisType]
+) -> TierkreisFunction:
     return TierkreisFunction(
-        f_tree.children[0].value,
+        name,
         TypeScheme({}, [], GraphType(Row(content=inputs), Row(content=outputs))),
         "",
     )
 
 
-def get_graph(f_tree, func_defs: FuncDefs) -> TierkreisGraph:
+def get_tkfunc_def(f_tree) -> TierkreisFunction:
+    inputs, outputs = get_inp_out(f_tree)
+    return gen_tkfunc(f_tree.children[0].value, inputs, outputs)
+
+
+def make_outports(node_ref: NodeRef, ports: Iterable[str]) -> List[NodePort]:
+    return [node_ref[outport] for outport in ports]
+
+
+def ports_from_tkfunc(func: TierkreisFunction, outputs: bool = True) -> List[str]:
+    # FIXME canonical ordering
+    row = func.type_scheme.body.outputs if outputs else func.type_scheme.body.inputs
+    return list(row.content.keys())
+
+
+def get_graph(f_tree, context: Context) -> TierkreisGraph:
     tg = TierkreisGraph()
     code_block = f_tree.children[-1]
-
-    node_names: Dict[str, Tuple[NodeRef, Optional[TierkreisFunction]]] = {}
-
     inputs, outputs = get_inp_out(f_tree)
+    context.inputs = inputs
+    context.outputs = outputs
+    append_code_block(code_block, context, tg)
 
-    def get_func_outputs(node_name: str):
-        node_ref, func = node_names[node_name]
-        outs = (
-            ["value"] if func is None else func.type_scheme.body.outputs.content.keys()
-        )
-        # FIXME canonical ordering
-        return [node_ref[outport] for outport in outs]
+    return tg
+
+
+def append_code_block(code_block: Tree, context: Context, tg: TierkreisGraph) -> None:
+
+    context = context.copy()
+
+    def get_func_outputs(node_name: str) -> List[NodePort]:
+        node_ref, func = context.output_vars[node_name]
+        outs = ports_from_tkfunc(func)
+        return make_outports(node_ref, outs)
 
     def get_outport(token) -> List[NodePort]:
         if token.data == "name":
             name = token.children[0].value
-            if name in inputs:
+            if name in context.inputs:
                 return [tg.input[name]]
-            if name in node_names:
+            if name in context.output_vars:
                 return get_func_outputs(name)
-            if name in func_defs:
-                grap, _ = func_defs[name]
+            if name in context.functions:
+                grap, _ = context.functions[name]
                 const_node = tg.add_const(grap)
-                node_names[const_node.name] = (const_node, None)
                 return [const_node["value"]]
+            if name in context.constants:
+                return [context.constants[name]["value"]]
             raise RuntimeError(f"Name not found in scope: {name}.")
         if token.data == "node_output":
-            return [node_names[token.children[0].value][0][token.children[1].value]]
+            return [
+                context.output_vars[token.children[0].value][0][token.children[1].value]
+            ]
         if token.data == "nested":
             node_ref, fun = add_node(token.children[0])
-            node_names[node_ref.name] = (node_ref, fun)
-            return get_func_outputs(node_ref.name)
+            return make_outports(
+                node_ref,
+                ports_from_tkfunc(
+                    fun,
+                ),
+            )
         if token.data == "const_port":
-            node_ref, fun = add_const_node(token.children[0])
-            node_names[node_ref.name] = (node_ref, fun)
+            node_ref = add_const_node(token.children[0])
             return [node_ref["value"]]
         raise RuntimeError
 
@@ -139,10 +205,9 @@ def get_graph(f_tree, func_defs: FuncDefs) -> TierkreisGraph:
 
         return dict(zip(expected_ports, all_outports))
 
-    def get_named_map_args(token) ->  Dict[str, NodePort]:
+    def get_named_map_args(token) -> Dict[str, NodePort]:
         return {
-            t.children[0].value: get_outport(t.children[1])[0]
-            for t in token.children
+            t.children[0].value: get_outport(t.children[1])[0] for t in token.children
         }
 
     def get_arglist(token, expected_ports: List[str]) -> Dict[str, NodePort]:
@@ -152,16 +217,14 @@ def get_graph(f_tree, func_defs: FuncDefs) -> TierkreisGraph:
             return get_positional_args(token, expected_ports)
         raise RuntimeError
 
-    def add_const_node(
-        token, name: Optional[str] = None
-    ) -> Tuple[NodeRef, Optional[TierkreisFunction]]:
+    def add_const_node(token, name: Optional[str] = None) -> NodeRef:
         val = get_const(token)
         const_node = tg.add_const(val, name)
-        return (const_node, None)
+        return const_node
 
     def add_node(
         token, name: Optional[str] = None
-    ) -> Tuple[NodeRef, Optional[TierkreisFunction]]:
+    ) -> Tuple[NodeRef, TierkreisFunction]:
         if token.data == "thunk":
             outport = token.children[0]
             thunk_port = get_outport(outport)[0]
@@ -174,8 +237,8 @@ def get_graph(f_tree, func_defs: FuncDefs) -> TierkreisGraph:
             try:
                 tk_func = sig[nmspace][fname]
             except KeyError as err:
-                if fname in func_defs:
-                    tk_func = func_defs[fname][1]
+                if fname in context.functions:
+                    tk_func = context.functions[fname][1]
                     primitive = False
                 else:
                     raise RuntimeError(f"Function name not found: {fname}") from err
@@ -186,72 +249,126 @@ def get_graph(f_tree, func_defs: FuncDefs) -> TierkreisGraph:
             if primitive:
                 noderef = tg.add_node(tk_func.name, name, **arglist)
             else:
-                noderef = tg.add_box(func_defs[fname][0], fname, **arglist)
+                noderef = tg.add_box(context.functions[fname][0], fname, **arglist)
             return (noderef, tk_func)
 
     for inst in code_block.children:
+        inst = cast(Tree, inst)
         if inst.data == "comment":
             pass
         elif inst.data == "output":
-            tg.set_outputs(**get_arglist(inst.children[0], list(outputs.keys())))
+            tg.set_outputs(
+                **get_arglist(inst.children[0], list(context.outputs.keys()))
+            )
         elif inst.data == "node":
-            node_names[inst.children[1]] = add_node(inst.children[0])
+            outvar = cast(str, inst.children[1])
+            node_inputs = cast(Tree, inst.children[0])
+            context.output_vars[outvar] = add_node(node_inputs)
         elif inst.data == "const_decl":
             target_name, token = inst.children
-            node_names[target_name] = add_const_node(token, target_name)
+            target_name = cast(str, target_name)
+            token = cast(Token, token)
+            context.constants[target_name] = add_const_node(token, target_name)
+        elif inst.data == "if_block":
+            condition = cast(Tree, inst.children[0])
+            pred = get_outport(condition)[0]
+
+            port_map = cast(Tree, inst.children[1])
+            inps = get_named_map_args(port_map)
+            loopcontext = Context()
+            loopcontext.functions = context.functions
+            loopcontext.inputs = {inp: VarType("unknown_if") for inp in inps}
+
+            # outputs from if-else block have to be named map (not positional)
+
+            if_block = cast(Tree, inst.children[2])
+            if_g = TierkreisGraph()
+            append_code_block(if_block, loopcontext, if_g)
+
+            else_block = cast(Tree, inst.children[3])
+            else_g = TierkreisGraph()
+            append_code_block(else_block, loopcontext, else_g)
+
+            output_var = cast(str, inst.children[4])
+
+            sw_nod = tg.add_node(
+                "builtin/switch", predicate=pred, true=if_g, false=else_g
+            )
+            eval_n = tg.add_node("builtin/eval", thunk=sw_nod["value"], **inps)
+
+            output_names = set(if_g.outputs()).union(else_g.outputs())
+            loopcontext.outputs = {outp: VarType("unknown_if") for outp in output_names}
+
+            fake_func = gen_tkfunc(
+                f"eval_{eval_n.name}", loopcontext.inputs, loopcontext.outputs
+            )
+            context.output_vars[output_var] = (eval_n, fake_func)
+            # raise RuntimeError
+        elif inst.data == "loop":
+
+            port_map = cast(Tree, inst.children[0])
+            inps = get_named_map_args(port_map)
+            loopcontext = Context()
+            loopcontext.functions = context.functions
+            loopcontext.inputs = {inp: VarType("unknown_loop") for inp in inps}
+
+            # outputs from if-else block have to be named map (not positional)
+
+            condition_block = cast(Tree, inst.children[1])
+            condition_g = TierkreisGraph()
+            append_code_block(condition_block, loopcontext, condition_g)
+
+            body_block = cast(Tree, inst.children[2])
+            body_g = TierkreisGraph()
+            append_code_block(body_block, loopcontext, body_g)
+
+            output_var = cast(str, inst.children[3])
+
+            loop_nod = tg.add_node(
+                "builtin/loop", condition=condition_g, body=body_g, **inps
+            )
+
+            loopcontext.outputs = {
+                outp: VarType("unknown_if") for outp in body_g.outputs()
+            }
+
+            fake_func = gen_tkfunc(
+                f"loop_{loop_nod.name}", loopcontext.inputs, loopcontext.outputs
+            )
+            context.output_vars[output_var] = (loop_nod, fake_func)
+            # raise RuntimeError
+
         else:
             pass
-    return tg
-
 
 
 if __name__ == "__main__":
     with open("tksl.lark") as f:
         parser = Lark(f.read())
 
-    text = """
-
-    def add2(x: int) -> (y: int) {
-        output(y: python_nodes::add(x, 2))
-    }
-
-    def add5(x: int) -> (y: int) {
-        output(y: python_nodes::add(x, 5))
-    }
-
-    def main(v1: int, v2: pair<int, bool>) -> (o1: int, o2:int) {
-        unpack_pair(v2) -> unp
-        const three = 3
-        python_nodes::add(a:unp.first, b:three) -> sum
-        make_pair(unp.second, "asdf") -> pair
-        # python_nodes::add(unp) -> sum
-        python_nodes::add(copy(v1)) -> total
-
-        eval(thunk: add2, x: total.value) -> eval_out
-        add5(sum) -> sum
-        output(o1:eval_out.y, o2: sum.y)
-    }
-
-    """
     # TODO inline function definitions
     with open(sys.argv[1]) as source:
         parse_tree = parser.parse(source.read())
 
-    func_defs: FuncDefs = {
+    context = Context()
+
+    funcs = [child.children[0] for child in parse_tree.children if child.data == "func"]
+    context.functions = {
         child.children[0].value: (TierkreisGraph(), get_tkfunc_def(child))
-        for child in parse_tree.children
+        for child in funcs
     }
 
     # funcs = TierkreisFunction(nam, TypeScheme({}, [], GraphType(Row(content=))))
-    for f_def in parse_tree.children:
+    for f_def in funcs:
         name = f_def.children[0].value
-        _, tkfunc = func_defs[name]
-        func_defs[name] = (get_graph(f_def, func_defs), tkfunc)
+        _, tkfunc = context.functions[name]
+        context.functions[name] = (get_graph(f_def, context), tkfunc)
 
-    tg = func_defs["main"][0]
+    tg = context.functions["main"][0]
 
-
-    with local_runtime("../../../../target/debug/tierkreis-server", show_output=True) as client:
+    with local_runtime(
+        "../../../../target/debug/tierkreis-server", show_output=True
+    ) as client:
 
         tg = client.type_check_graph_blocking(tg)
 

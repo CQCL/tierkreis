3c2bde4f4e13800976eeec10c22e6df4985d404d
seyon <seyon.sivarajah@cambridgequantum.com>
2021-11-17 10:21:29 +0000

    allow python testing against remote server

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,12 @@ from typing import AsyncIterator
 
 import pytest
 
-from tierkreis.frontend import RuntimeClient, local_runtime, DockerRuntime
+from tierkreis.frontend import (
+    RuntimeClient,
+    local_runtime,
+    DockerRuntime,
+    myqos_runtime,
+)
 
 from . import LOCAL_SERVER_PATH
 
@@ -13,6 +18,11 @@ def pytest_addoption(parser):
         "--docker",
         action="store_true",
         help="Whether to use docker container for server rather than local binary",
+    )
+    parser.addoption(
+        "--myqos",
+        action="store_true",
+        help="Whether to use the myqos runtime for testing",
     )
     parser.addoption(
         "--server-logs",
@@ -32,8 +42,10 @@ def event_loop(request):
 async def client(request) -> AsyncIterator[RuntimeClient]:
     isdocker = False
     logs = False
+    ismyqos = False
     try:
         isdocker = request.config.getoption("--docker") not in (None, False)
+        ismyqos = request.config.getoption("--myqos") not in (None, False)
         logs = request.config.getoption("--server-logs") not in (None, False)
     except Exception as _:
         pass
@@ -43,6 +55,12 @@ async def client(request) -> AsyncIterator[RuntimeClient]:
             "cqc/tierkreis",
         ) as local_client:
             yield local_client
+    elif ismyqos:
+        async with myqos_runtime(
+            "tierkreistrr595bx-pr.uksouth.cloudapp.azure.com",
+            staging_creds=True,
+        ) as myqos_client:
+            yield myqos_client
     else:
         # launch a local server for this test run and kill it at the end
         async with local_runtime(LOCAL_SERVER_PATH, show_output=logs) as local_client:
--- tierkreis/frontend/__init__.py
+++ tierkreis/frontend/__init__.py
@@ -2,3 +2,4 @@
 from .runtime_client import RuntimeClient
 from .docker_manager import DockerRuntime
 from .local_manager import local_runtime
+from .myqos_client import myqos_runtime

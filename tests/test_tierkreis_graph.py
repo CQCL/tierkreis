CQCL/tierkreis

commit ad44093399d987fe84ac021b1ba245490811d2ac

Remove Option, including from TKSL (#182)
* Rust and protobuf: remove Option (Type and Value)
* Remove builtin/make_option and builtin/unwrap
* python: convert Optional to Variant with Some/None tags, including roundtrip of python None
* TKSL: remove TYPE_OPTION and all option support
* ....and update option.tksl (which calls python) with explicit Variant

Note, the python interface will have trouble if the user has tags Some or None in Variant types that do *not* map to Optional.

tierkreis/core/types.py
@@ -12,6 +12,8 @@ import tierkreis.core.protos.tierkreis.signature as ps
 from tierkreis.core.internal import python_struct_fields
 from tierkreis.core.tierkreis_struct import TierkreisStruct
 
+from . import Labels
+
 # import from types when updating to python 3.10
 try:
     from types import NoneType  # type: ignore
@@ -66,7 +68,12 @@ class TierkreisType(ABC):
         elif type_ is float:
             result = FloatType()
         elif inner_type := _get_optional_type(type_):
-            result = OptionType(TierkreisType.from_python(inner_type, visited_types))
+            inner = TierkreisType.from_python(inner_type, visited_types)
+            return VariantType(
+                shape=Row(
+                    content={Labels.NONE: StructType(shape=Row()), Labels.SOME: inner}
+                )
+            )
         elif type_origin is list:
             args = typing.get_args(type_)
             result = VecType(element=TierkreisType.from_python(args[0], visited_types))
@@ -124,9 +131,6 @@ class TierkreisType(ABC):
             result = StringType()
         elif name == "flt":
             result = FloatType()
-        elif name == "option":
-            inner = TierkreisType.from_proto(cast(pg.Type, out_type))
-            result = OptionType(inner)
         elif name == "var":
             result = VarType(cast(str, out_type))
         elif name == "pair":
@@ -172,20 +176,6 @@ class TierkreisType(ABC):
             (child.contained_vartypes() for child in self.children()),
             set(),
         )
-
-
-@dataclass
-class OptionType(TierkreisType):
-    inner: TierkreisType
-
-    def to_proto(self) -> pg.Type:
-        return pg.Type(option=self.inner.to_proto())
-
-    def __str__(self) -> str:
-        return f"Option<{str(self.inner)}>"
-
-    def children(self) -> list["TierkreisType"]:
-        return [self.inner]
 
 
 @dataclass

tests/test_tierkreis_graph.py
@@ -15,11 +15,11 @@ from tierkreis.core.values import (
     FloatValue,
     IntValue,
     MapValue,
-    OptionValue,
     PairValue,
     TierkreisValue,
     TierkreisVariant,
     VecValue,
+    option_some,
 )
 
 
@@ -110,6 +110,7 @@ def test_value_topython():
         ("a", 4),
         {"asf": 3, "fsd": 4},
         TierkreisVariant("mylabel", 5),
+        None,
     )
 
     for val in convertible_vals:
@@ -119,7 +120,7 @@ def test_value_topython():
     for val in fail_vals:
         assert TierkreisValue.from_python(val).try_autopython() is None
 
-    assert OptionValue(VecValue([IntValue(3), IntValue(4)])).try_autopython() == [3, 4]
+    assert option_some(VecValue([IntValue(3), IntValue(4)])).try_autopython() == [3, 4]
 
 
 @pytest.mark.xfail(raises=AssertionError)

tests/test_pyruntime.py
@@ -2,7 +2,7 @@ import pytest
 
 from tierkreis.core import Labels
 from tierkreis.core.tierkreis_graph import TierkreisEdge, TierkreisGraph
-from tierkreis.core.values import OptionValue, StringValue, TierkreisValue, VariantValue
+from tierkreis.core.values import TierkreisValue, VariantValue
 from tierkreis.frontend import python_builtin
 from tierkreis.frontend.python_runtime import PyRuntime
 from tierkreis.frontend.runtime_client import ServerRuntime
@@ -68,9 +68,6 @@ def sample_graph() -> TierkreisGraph:
             other=2,
         ),
         loop_out=tg.add_func("builtin/loop", body=_loop_graph(), value=2)["value"],
-        option=tg.add_func(
-            "builtin/unwrap", option=OptionValue(StringValue("inside_option"))
-        ),
     )
     return tg
 

tests/tksl_samples/option.tksl
@@ -1,11 +1,11 @@
 use python_nodes::test_option;
 
-Graph id_option(inp: Option<Int>) -> (out: Option<Int>) {
+Graph id_option(inp: Variant<Some:Int|None:Struct<>>) -> (out: Variant<Some:Int|None:Struct<>>) {
     output(out: inp);
 }
 
 
 Graph main() -> (none: Int, some: Int) {
 
-    output (none: test_option(id_option(None)), some: test_option(Some(30)));
+    output (none: test_option(id_option(tag(None:Struct{}))), some: test_option(tag(Some:30)));
 }

tests/worker_test/main.py
@@ -69,6 +69,8 @@ async def fail() -> FailOutput:
 async def test_option(x: Optional[int]) -> int:
     if x is None:
         return -1
+    # Just check the runtime values correspond with the type annotations
+    assert isinstance(x, int)
     return x
 
 

tierkreis/core/__init__.py
@@ -1,6 +1,3 @@
-from .tierkreis_graph import TierkreisGraph
-
-
 class Labels:
     def __init__(self):
         raise RuntimeError("Do not instantiate")
@@ -10,3 +7,11 @@ class Labels:
     VARIANT_VALUE = "variant_value"
     CONTINUE = "continue"
     BREAK = "break"
+    # These are not set by the protobuf/Tierkreis framework,
+    # but used by the python frontend to map to python 'Optional'
+    SOME = "Some"
+    NONE = "None"
+
+
+# This needs to be here to fix some cycles in the imports
+from .tierkreis_graph import TierkreisGraph  # pylint: disable=wrong-import-position

tierkreis/core/values.py
@@ -11,7 +11,9 @@ import betterproto
 import tierkreis.core.protos.tierkreis.graph as pg
 from tierkreis.core.internal import python_struct_fields
 from tierkreis.core.tierkreis_struct import TierkreisStruct
-from tierkreis.core.types import _get_optional_type
+from tierkreis.core.types import NoneType, _get_optional_type
+
+from . import Labels
 
 T = typing.TypeVar("T")
 
@@ -30,13 +32,6 @@ class ToPythonFailure(Exception):
 
     def __str__(self) -> str:
         return f"Value {self.value} conversion to python type failed."
-
-
-class NoneConversion(IncompatiblePyType):
-    def __str__(self) -> str:
-        return (
-            super().__str__() + "\nFor None values try using OpionValue(None) directly."
-        )
 
 
 class TierkreisValue(ABC):
@@ -96,6 +91,8 @@ class TierkreisValue(ABC):
             return GraphValue(value.graph)
         if isinstance(value, bool):
             find_subclass = cls._pytype_map[bool]
+        if value is None:
+            return option_none
         else:
             try:
                 find_subclass = next(
@@ -109,13 +106,14 @@ class TierkreisValue(ABC):
                     # might still be convertible to Struct depending on field types
                     find_subclass = StructValue
                 else:
-                    exception = (
-                        NoneConversion(value)
-                        if value is None
-                        else IncompatiblePyType(value)
-                    )
-                    raise exception from e
+                    raise IncompatiblePyType(value) from e
         return find_subclass.from_python(value)
+
+    @classmethod
+    def from_python_optional(cls, val: Optional[Any]) -> "TierkreisValue":
+        if val is None:
+            return option_none
+        return option_some(cls.from_python(val))
 
     def try_autopython(self) -> Optional[Any]:
         """Try to automatically convert to a python type without specifying the
@@ -130,58 +128,6 @@ class TierkreisValue(ABC):
             return self.to_python(self._instance_pytype)
         except ToPythonFailure as _:
             return None
-
-
-@dataclass(frozen=True)
-class OptionValue(TierkreisValue):
-    _proto_name: ClassVar[str] = "option"
-    _class_pytype: ClassVar[typing.Type] = Optional  # type: ignore
-    inner: Optional[TierkreisValue]
-
-    @property
-    def _instance_pytype(self):
-        return Optional[
-            TypeVar("C") if self.inner is None else self.inner._instance_pytype
-        ]
-
-    def to_proto(self) -> pg.Value:
-        optval = (
-            pg.OptionValue(value=self.inner.to_proto())
-            if self.inner
-            else pg.OptionValue()
-        )
-        return pg.Value(option=optval)
-
-    def to_python(self, type_: typing.Type[T]) -> T:
-        if isinstance(type_, typing.TypeVar):
-            return cast(T, self)
-        if self.inner is None:
-            return cast(T, None)
-        if inner_type := _get_optional_type(type_):
-            return cast(T, self.inner.to_python(inner_type))
-        raise ToPythonFailure(self)
-
-    @classmethod
-    def from_python(cls, value: Any) -> "TierkreisValue":
-        value = cast(Optional[Any], value)
-        optval = None if value is None else TierkreisValue.from_python(value)
-        return OptionValue(optval)
-
-    @classmethod
-    def from_proto(cls, value: pg.OptionValue) -> "TierkreisValue":
-        name, val = betterproto.which_one_of(value, "inner")
-        if name == "value":
-            return OptionValue(
-                TierkreisValue.from_proto(val),
-            )
-        else:
-            return OptionValue(None)
-
-    def __str__(self) -> str:
-        return f"Option({str(self.inner)})"
-
-    def to_tksl(self) -> str:
-        return f"Some({self.inner.to_tksl()})" if self.inner else "None"
 
 
 @dataclass(frozen=True)
@@ -527,7 +473,7 @@ class StructValue(TierkreisValue):
 
         return StructValue(
             {
-                name: OptionValue.from_python(value)
+                name: TierkreisValue.from_python_optional(value)
                 if _get_optional_type(types[name])
                 else TierkreisValue.from_python(value)
                 for name, value in vals.items()
@@ -571,6 +517,10 @@ class VariantValue(TierkreisValue):
 
     @property
     def _instance_pytype(self):
+        if self.tag == Labels.SOME:
+            return Optional[self.value._instance_pytype]
+        if self == option_none:
+            return Optional[Any]
         return TierkreisVariant[self.value._instance_pytype]
 
     def to_proto(self) -> pg.Value:
@@ -582,8 +532,13 @@ class VariantValue(TierkreisValue):
         if isinstance(type_, typing.TypeVar):
             return cast(T, self)
         if typing.get_origin(type_) is TierkreisVariant:
-            (type_args,) = typing.get_args(type_)
-            return cast(T, TierkreisVariant(self.tag, self.value.to_python(type_args)))
+            (type_arg,) = typing.get_args(type_)
+            return cast(T, TierkreisVariant(self.tag, self.value.to_python(type_arg)))
+        elif self == option_none:
+            assert type_ is NoneType or _get_optional_type(type_) is not None
+            return cast(T, None)
+        elif (inner_t := _get_optional_type(type_)) and self.tag == Labels.SOME:
+            return cast(T, self.value.to_python(inner_t))
         raise ToPythonFailure(self)
 
     def to_tksl(self) -> str:
@@ -596,3 +551,10 @@ class VariantValue(TierkreisValue):
     @classmethod
     def from_proto(cls, value: pg.VariantValue) -> "TierkreisValue":
         return VariantValue(value.tag, TierkreisValue.from_proto(value.value))
+
+
+option_none = VariantValue(Labels.NONE, StructValue({}))
+
+
+def option_some(inner: TierkreisValue) -> TierkreisValue:
+    return VariantValue(Labels.SOME, inner)

tierkreis/frontend/python_builtin.py
@@ -2,7 +2,7 @@
 
 from copy import deepcopy
 from dataclasses import dataclass
-from typing import Generic, Optional, TypeVar, cast
+from typing import Generic, TypeVar, cast
 
 from tierkreis.core.tierkreis_graph import GraphValue, IncomingWireType, TierkreisGraph
 from tierkreis.core.tierkreis_struct import TierkreisStruct
@@ -240,17 +240,6 @@ namespace.functions["loop"] = Function(
 
 
 @dataclass
-class _OptOut(TierkreisStruct, Generic[a]):
-    value: Optional[a]
-
-
-@namespace.function(type_vars={"a": StarKind()})
-async def make_option(value: a) -> _OptOut[a]:
-    "Make option from value"
-    return _OptOut(value)
-
-
-@dataclass
 class _MakePairOut(TierkreisStruct, Generic[a, b]):
     pair: tuple[a, b]
 
@@ -378,14 +367,6 @@ async def _unpack_struct(ins: StructValue) -> StructValue:
 namespace.functions["unpack_struct"] = Function(
     run=_unpack_struct, declaration=_builtin_defs.functions["builtin/unpack_struct"]
 )
-
-
-@namespace.function(type_vars={"a": StarKind()})
-async def unwrap(option: Optional[a]) -> a:
-    """Get the item out of an Option"""
-    if option is None:
-        raise RuntimeError("Attempt to unwrap a None option.")
-    return option
 
 
 @namespace.function()

commit 24dadaa284cf9b8730eeefe0a00023cd893df065

Fix handling of cyclic types containing "Optional"  (#196)
* Fix TierkreisType.from_python cycle handling of optional types (broken in #182)
* Add test_type_from_python

tierkreis/core/types.py
@@ -69,7 +69,7 @@ class TierkreisType(ABC):
             result = FloatType()
         elif inner_type := _get_optional_type(type_):
             inner = TierkreisType.from_python(inner_type, visited_types)
-            return VariantType(
+            result = VariantType(
                 shape=Row(
                     content={Labels.NONE: StructType(shape=Row()), Labels.SOME: inner}
                 )

tests/test_tierkreis_graph.py
@@ -1,8 +1,9 @@
-from typing import Any, Iterable
+from dataclasses import dataclass
+from typing import Any, Iterable, Optional
 
 import pytest
 
-from tierkreis.core import TierkreisGraph
+from tierkreis.core import Labels, TierkreisGraph
 from tierkreis.core.tierkreis_graph import (
     FunctionNode,
     NodePort,
@@ -10,7 +11,17 @@ from tierkreis.core.tierkreis_graph import (
     TierkreisEdge,
     TierkreisFunction,
 )
-from tierkreis.core.types import GraphType, IntType, Row, TypeScheme
+from tierkreis.core.tierkreis_struct import TierkreisStruct
+from tierkreis.core.types import (
+    GraphType,
+    IntType,
+    Row,
+    StructType,
+    TierkreisType,
+    TypeScheme,
+    VariantType,
+    VarType,
+)
 from tierkreis.core.values import (
     FloatValue,
     IntValue,
@@ -121,6 +132,37 @@ def test_value_topython():
         assert TierkreisValue.from_python(val).try_autopython() is None
 
     assert option_some(VecValue([IntValue(3), IntValue(4)])).try_autopython() == [3, 4]
+
+
+@dataclass
+class Foo(TierkreisStruct):
+    head: int
+    tail: Optional["Foo"]
+
+
+def test_type_from_python():
+    t = TierkreisType.from_python(Foo)
+    assert isinstance(t, StructType)
+    fields = t.shape.content
+    tail = fields.pop("tail")
+    assert isinstance(tail, VariantType)
+    some = tail.shape.content[Labels.SOME]
+    assert isinstance(some, VarType) and some.name.startswith("CyclicType")
+    assert fields == {"head": IntType()}
+
+    @dataclass
+    class Bar(TierkreisStruct):
+        data1: Optional[int]
+        data2: Optional[int]
+
+    t = TierkreisType.from_python(Bar)
+    assert isinstance(t, StructType)
+    fields = t.shape.content
+    d1 = fields.pop("data1")
+    assert isinstance(d1, VariantType)
+    assert d1.shape.content.keys() == frozenset([Labels.SOME, Labels.NONE])
+    assert d1.shape.content[Labels.SOME] == IntType()
+    assert fields == {"data2": d1}
 
 
 @pytest.mark.xfail(raises=AssertionError)
d4f1d576863cd439538737136d29f48e13ecb291
seyon <seyon.sivarajah@cambridgequantum.com>
2022-10-25 17:42:13 +0100

    export render_graph at top level

--- tierkreis/__init__.py
+++ tierkreis/__init__.py
@@ -1,4 +1,4 @@
 """Tierkreis python tools"""
 # only import elements from core package
-from tierkreis.core.graphviz import tierkreis_to_graphviz
+from tierkreis.core.graphviz import render_graph, tierkreis_to_graphviz
 from tierkreis.core.tierkreis_graph import TierkreisGraph, TierkreisType, TierkreisValue

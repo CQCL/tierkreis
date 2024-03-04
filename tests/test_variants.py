c88805b9596863cd72c18dba9503b79db1757289
Alan Lawrence <alan.lawrence@quantinuum.com>
2024-03-04 15:21:26 +0000

    Move tests into test_generics.py, add test_pydantic_nested (#439)
    New test documents behaviour, including of pydantic, but it all looks reasonable

--- tests/test_variants.py
+++ tests/test_variants.py
@@ -6,25 +6,17 @@ import pydantic as pyd
 import pytest
 from test_worker.main import (
     IntStruct,
-    MyGeneric,
-    MyGenericList,
 )
 
 from tierkreis.builder import Const, Output, UnionConst, graph
 from tierkreis.client.runtime_client import RuntimeClient
 from tierkreis.core.opaque_model import OpaqueModel
 from tierkreis.core.types import (
-    IntType,
-    Row,
-    StructType,
     TierkreisType,
     UnionTag,
-    VarType,
-    VecType,
 )
 from tierkreis.core.values import (
     FloatValue,
-    IncompatibleAnnotatedValue,
     IntValue,
     StringValue,
     StructValue,
@@ -283,86 +275,3 @@ async def test_pydantic_types(bi, client: RuntimeClient) -> None:
         out = (await client.run_graph(g))["value"]
 
         assert out.to_python(py_type) == py_val
-
-
-@pytest.mark.asyncio
-async def test_generic(bi, client: RuntimeClient) -> None:
-    py_type = MyGeneric[int]
-    py_val = MyGeneric(x=1)
-    tk_val = StructValue(
-        {
-            "x": IntValue(1),
-        }
-    )
-    tk_ty = StructType(shape=Row({"x": IntType()}), name="MyGeneric[int]")
-
-    assert TierkreisType.from_python(py_type) == tk_ty
-    assert TierkreisValue.from_python(py_val, py_type) == tk_val
-    with pytest.raises(IncompatibleAnnotatedValue, match="~G"):
-        # value doesn't know it is instance of concrete type so fails.
-        TierkreisValue.from_python(py_val)
-    assert tk_val.to_python(py_type) == py_val
-
-    pn = bi["python_nodes"]
-
-    @graph()
-    def g() -> Output:
-        return Output(pn.generic_int_to_str(Const(py_val, py_type)))
-
-    out = (await client.run_graph(g))["value"]
-
-    assert out.to_python(MyGeneric[str]) == MyGeneric(x="got number: 1")
-
-
-@pytest.mark.asyncio
-async def test_generic_list(bi, client: RuntimeClient) -> None:
-    py_type = MyGenericList[int]
-    py_val = MyGenericList(x=[1])
-    tk_val = StructValue(
-        {
-            "x": VecValue([IntValue(1)]),
-        }
-    )
-    tk_ty = StructType(shape=Row({"x": VecType(IntType())}), name="MyGenericList[int]")
-
-    assert TierkreisType.from_python(py_type) == tk_ty
-    assert TierkreisValue.from_python(py_val, py_type) == tk_val
-    with pytest.raises(IncompatibleAnnotatedValue, match="~G"):
-        # value doesn't know it is instance of concrete type so fails.
-        TierkreisValue.from_python(py_val)
-    assert tk_val.to_python(py_type) == py_val
-
-    pn = bi["python_nodes"]
-
-    @graph()
-    def g() -> Output:
-        return Output(pn.generic_int_to_str_list(Const(py_val, py_type)))
-
-    out = (await client.run_graph(g))["value"]
-
-    assert out.to_python(MyGenericList[str]) == MyGenericList(x=["got number: 1"])
-
-
-@pytest.mark.xfail(reason="generic dataclasses aren't supported - only pydantic models")
-def test_generic_dataclass() -> None:
-    from dataclasses import dataclass
-    from typing import Generic, TypeVar
-
-    T = TypeVar("T")
-
-    @dataclass
-    class MyGenericDataclass(Generic[T]):
-        x: T
-
-    assert TierkreisType.from_python(MyGenericDataclass[int]) == StructType(
-        shape=Row({"x": VarType("T")}), name="MyGenericDataclass"
-    )
-    tk_val = StructValue(
-        {
-            "x": IntValue(1),
-        }
-    )
-    py_val = MyGenericDataclass(x=1)
-    # both asserts below fail
-    assert tk_val.to_python(MyGenericDataclass[int]) == py_val
-    assert TierkreisValue.from_python(py_val, MyGenericDataclass[int]) == tk_val

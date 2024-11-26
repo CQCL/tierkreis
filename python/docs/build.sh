b722aafd76e560b097f682fc8b381c7c46064cd0
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-11-26 11:40:09 +0000

    chore: fix docs building (#19)

--- python/docs/build.sh
+++
@@ -1,5 +0,0 @@
-mkdir build
-
-touch build/.nojekyll  # Disable jekyll to keep files starting with underscores
-
-sphinx-build -b html ./api-docs ./build/api-docs
---
+++ scripts/build_py_docs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# run from root directory
+export DOCS_DIR=$(pwd)/python/docs
+mkdir -p $DOCS_DIR/build
+
+touch $DOCS_DIR/build/.nojekyll  # Disable jekyll to keep files starting with underscores
+
+sphinx-build -b html $DOCS_DIR/api-docs $DOCS_DIR/build/api-docs

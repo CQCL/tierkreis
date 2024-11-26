073084391d076cca38759182d8cffc8019d5a430
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-11-26 11:19:50 +0000

    re target docs dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
-mkdir build
+# run from root directory
+export DOCS_DIR=$(pwd)/python/docs
+mkdir -p $DOCS_DIR/build
 
-touch build/.nojekyll  # Disable jekyll to keep files starting with underscores
+touch $DOCS_DIR/build/.nojekyll  # Disable jekyll to keep files starting with underscores
 
-sphinx-build -b html ./api-docs ./build/api-docs
+sphinx-build -b html $DOCS_DIR/api-docs $DOCS_DIR/build/api-docs

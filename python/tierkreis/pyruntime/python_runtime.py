ef9deb2df1bd79c491a864e1a80c0993c44a6d42
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2025-02-12 17:37:48 +0000

    Rewrite PyRuntime, dropping RuntimeClient support - pyright OK for file only

--- python/tierkreis/pyruntime/python_runtime.py
+++ python/tierkreis/pyruntime/python_runtime.py
@@ -2,63 +2,30 @@
 
 import asyncio
 from copy import deepcopy
-from typing import TYPE_CHECKING, Any, Callable, Iterable, Optional, Tuple, cast
+from typing import TYPE_CHECKING, Any, Callable, Iterable, Optional, Tuple, Sequence
 
 import networkx as nx
 import requests
 
-from tierkreis.client.runtime_client import RuntimeClient
-from tierkreis.core import Labels
-from tierkreis.core.function import FunctionName
-from tierkreis.core.protos.tierkreis.v1alpha1.graph import Output, OutputStream
-from tierkreis.core.signature import Signature
-from tierkreis.core.tierkreis_graph import (
-    BoxNode,
-    ConstNode,
-    FunctionNode,
-    GraphValue,
-    IncomingWireType,
-    InputNode,
-    MatchNode,
-    OutputNode,
-    TagNode,
-    TierkreisEdge,
-    TierkreisGraph,
-)
-from tierkreis.core.type_errors import TierkreisTypeErrors
-from tierkreis.core.type_inference import _TYPE_CHECK, infer_graph_types
-from tierkreis.core.utils import map_vals
-from tierkreis.core.values import StructValue, TierkreisValue, VariantValue, VecValue
+from hugr import Hugr, Node, InPort, OutPort, ops, tys
+from hugr.val import Sum, Value
+
 from tierkreis.pyruntime import python_builtin
 
 if TYPE_CHECKING:
-    from tierkreis.core.tierkreis_graph import _EdgeData
     from tierkreis.worker.namespace import Namespace
-
-
-class _ValueNotFound(Exception):
-    def __init__(self, edge: TierkreisEdge) -> None:
-        self.edge = edge
-        super().__init__(f"Value not found on edge {edge.source} -> {edge.target}")
-
-
-class OutputNotFound(_ValueNotFound):
-    """Node output expected but not found."""
-
-
-class InputNotFound(_ValueNotFound):
-    """Node input expected but not found."""
 
 
 class FunctionNotFound(Exception):
     """Function expected but not found."""
 
-    def __init__(self, fname: FunctionName) -> None:
+    def __init__(self, fname: str) -> None:
         self.function = fname
         super().__init__(f"Function {fname} not found in namespace.")
 
-
-class PyRuntime(RuntimeClient):
+# ALAN can we implement RuntimeClient somehow,
+# e.g. type_check -> we support all ops as in Hugr, and can run?
+class PyRuntime:
     """A simplified python-only Tierkreis runtime. Can be used with builtin
     operations and python only namespaces that are locally available."""
 
@@ -69,98 +36,138 @@ class PyRuntime(RuntimeClient):
         for root in roots:
             self.root.merge_namespace(root)
         self.num_workers = num_workers
-        self._callback: Optional[Callable[[TierkreisEdge, TierkreisValue], None]] = None
+        self._callback: Optional[Callable[[OutPort, Value], None]] = None
         self.set_callback(None)
 
     def set_callback(
-        self, callback: Optional[Callable[[TierkreisEdge, TierkreisValue], None]]
+        self, callback: Optional[Callable[[OutPort, Value], None]]
     ):
-        """Set a callback function that takes a TierkreisEdge and
-        TierkreisValue, which will be called every time a edge receives an
-        output. Can be used to inspect intermediate values."""
+        """Set a callback function that takes an OutPort and Value,
+        which will be called every time a value is output.
+        Can be used to inspect intermediate values."""
         self._callback = callback
 
     def callback(
         self,
-        edge: TierkreisEdge,
-        val: TierkreisValue,
+        out_port: OutPort,
+        val: Value,
     ):
         """If a callback function is set, call it with an edge and the value on
         the edge."""
         if self._callback:
-            self._callback(edge, val)
+            self._callback(out_port, val)
 
     async def run_graph(
-        self,
-        run_g: TierkreisGraph,
-        /,
-        **py_inputs: Any,
-    ) -> dict[str, TierkreisValue]:
+        self,         # ALAN next line we've changed sig, does ignore work?
+        run_g: Hugr,  # type:ignore
+        *py_inputs: Any,
+    ) -> list[Value]:
         """Run a tierkreis graph using the python runtime, and provided inputs.
-        Returns the outputs of the graph.
+           Returns the outputs of the graph.
         """
-        total_nodes = run_g._graph.number_of_nodes()
-        runtime_state: dict["_EdgeData", TierkreisValue] = {}
+        def make_val(v: Any) -> Value: # TODO take desired type also, e.g. int width
+            if isinstance(v, Value):
+                return v
+            raise RuntimeError("Don't know how to convert python value: {v}")
+        return await self._run_container(run_g, run_g.root, [make_val(p) for p in py_inputs])
 
-        async def run_node(node: int) -> dict[str, TierkreisValue]:
-            tk_node = run_g[node]
+    async def _run_container(
+        self,
+        run_g: Hugr,
+        parent: Node,
+        inputs: Sequence[Value]
+    ) -> list[Value]:
+        """ parent is a DataflowOp """
+        parent_node = run_g[parent]
+        if isinstance(parent_node, ops.DFG):
+            return await self._run_dataflow_subgraph(run_g, parent, inputs)
+        if isinstance(parent_node, ops.CFG):
+            pc = run_g.children(parent)[0]
+            assert isinstance(pc, ops.DataflowBlock)
+            while True:
+                (tag, inputs) = unpack_first(*await self._run_dataflow_subgraph(run_g, pc, inputs))
+                (bb,) = run_g.linked_ports(pc[tag])  # Should only be 1
+                pc = bb.node
+                if isinstance(pc, ops.ExitBlock):
+                    return inputs
+        if isinstance(parent_node, ops.Conditional):
+            (tag, inputs) = unpack_first(*inputs)
+            case_node = run_g.children(parent)[tag]
+            return await self._run_dataflow_subgraph(run_g, case_node, inputs)
+        if isinstance(parent_node, ops.TailLoop):
+            while True:
+                (tag, inputs) = unpack_first(*await self._run_dataflow_subgraph(run_g, parent, inputs))
+                if tag == ops.Break.tag:
+                    return inputs
+        raise RuntimeError("Unknown container type")
+    
+    async def _run_dataflow_subgraph(
+        self,
+        run_g: Hugr,
+        parent: Node,
+        inputs: Sequence[Value]
+    ) -> list[Value]:
+        #assert isinstance(run_g[parent], ops.DfParentOp)
+        # FuncDefn corresponds to a Call, but inputs are the arguments
+        total_nodes = len(run_g.children(parent))
+        runtime_state: dict[OutPort, Value] = {}
 
-            if isinstance(tk_node, OutputNode):
-                return {}
-            if isinstance(tk_node, InputNode):
-                return map_vals(py_inputs, TierkreisValue.from_python)
-
-            if isinstance(tk_node, ConstNode):
-                return {Labels.VALUE: tk_node.value}
-
-            in_edges = list(run_g.in_edges(node))
-            while not all(e.to_edge_handle() in runtime_state for e in in_edges):
-                # wait for inputs to become available
-                # only useful if there are other workers that can do things
-                # while this one waits
+        async def get_output(src: OutPort, wait: bool) -> Value:
+            while wait and (src not in runtime_state):
                 assert self.num_workers > 1
                 await asyncio.sleep(0)
-            try:
-                in_values = (
-                    (e, runtime_state.pop(e.to_edge_handle())) for e in in_edges
-                )
-            except KeyError as key_e:
-                raise InputNotFound(run_g._to_tkedge(key_e.args[0])) from key_e
+            return runtime_state[src]
 
-            inps = {e.target.port: val for e, val in in_values}
-            if isinstance(tk_node, FunctionNode):
-                fname = tk_node.function_name
-                if fname.namespaces == [] and fname.name == "eval":
-                    return await self._run_eval(inps)
-                elif fname.namespaces == [] and fname.name == "loop":
-                    return await self._run_loop(inps)
-                elif fname.namespaces == [] and fname.name == "map":
-                    return await self._run_map(inps)
-                else:
-                    function = self.root.get_function(fname)
-                    if function is None:
-                        raise FunctionNotFound(fname)
-                    # For now the PyRuntime does not provide a stack trace
-                    return (await function.run(self, dict(), StructValue(inps))).values
+        async def get_inputs(node: Node, wait: bool=True) -> list[Value]:
+            return [await get_output(outp, wait=wait)
+                for (inp, outps) in run_g.incoming_links(node)
+                if isinstance(run_g.port_kind(inp), tys.ValueKind)
+                for outp in outps # Should only be one for a ValueKind
+            ]   
 
-            elif isinstance(tk_node, BoxNode):
-                return await self.run_graph(
-                    tk_node.graph,
-                    **inps,
-                )
+        async def run_node(node: Node) -> list[Value]:
+            tk_node = run_g[node]
 
-            elif isinstance(tk_node, MatchNode):
-                return self._run_match(inps)
+            # ops.Input, ops.Output
+            # ops.Custom, ops.ExtOp, ops.RegisteredOp
+            # ops.MakeTuple, ops.UnpackTuple, ops.Tag
+            # ops.Const (IGNORE?), ops.LoadConst (find static input)
+            # ops.Conditional (select appropriate ops.Case) - just run_graph?
+            # ops.CFG, ops.DFG, ops.TailLoop - just run_graph?
+            # ops.FuncDefn - IGNORE, it's just a definition
 
-            elif isinstance(tk_node, TagNode):
-                return {
-                    Labels.VALUE: VariantValue(tk_node.tag_name, inps[Labels.VALUE])
-                }
+            # ops.Call, ops.CallIndirect, ops.LoadFunc
+            # IGNORE (as child): ops.FuncDefn, ops.Module (?),
 
+            if isinstance(tk_node, ops.Output):
+                return []
+            if isinstance(tk_node, ops.Input):
+                return list(inputs)
+
+            if isinstance(tk_node, (ops.Const, ops.FuncDefn, ops.FuncDecl, ops.AliasDefn, ops.AliasDecl)):
+                # These are static only, no value outputs
+                return []
+            if isinstance(tk_node, ops.LoadConst):
+                (const_src,) = run_g.linked_ports(InPort(node, 0))
+                cst = run_g[const_src.node]
+                assert isinstance(cst, ops.Const)
+                return [cst.val]
+
+            inps = await get_inputs(node, wait=True)
+            if isinstance(tk_node, (ops.Conditional, ops.CFG, ops.DFG, ops.TailLoop)):
+                return await self._run_container(run_g, node, inputs)
+            elif isinstance(tk_node, ops.Tag):
+                return [Sum(tk_node.tag, tk_node.sum_ty, inps)]
+            elif isinstance(tk_node, ops.MakeTuple):
+                return [Sum(0, tys.Sum([tk_node.types]), inps)]
+            elif isinstance(tk_node, ops.UnpackTuple):
+                (sum_val,) = inps
+                assert isinstance(sum_val, Sum)
+                return sum_val.vals
             else:
                 raise RuntimeError("Unknown node type.")
 
-        async def worker(queue: asyncio.Queue[int]):
+        async def worker(queue: asyncio.Queue[Node]):
             # each worker gets the next node in the queue
             while True:
                 node = await queue.get()
@@ -169,19 +176,16 @@ class PyRuntime(RuntimeClient):
                 outs = await run_node(node)
 
                 # assign outputs to edges
-                for out_edge in run_g.out_edges(node):
-                    try:
-                        val = outs.pop(out_edge.source.port)
-                    except KeyError as key_e:
-                        raise OutputNotFound(out_edge) from key_e
-                    tkval = TierkreisValue.from_python(val)
-                    self.callback(out_edge, tkval)
-                    runtime_state[out_edge.to_edge_handle()] = tkval
+                assert len(outs) == run_g.num_out_ports(node)
+                for (outport_idx, val) in enumerate(outs):
+                    outp = OutPort(node, outport_idx)
+                    self.callback(outp, val)
+                    runtime_state[outp] = val
                 # signal this node is now done
                 queue.task_done()
 
-        que: asyncio.Queue[int] = asyncio.Queue(total_nodes)
-        for node in nx.topological_sort(run_g._graph):
+        que: asyncio.Queue[Node] = asyncio.Queue(total_nodes)
+        for node in nx.topological_sort(run_g.children(parent)):
             # add all node names to the queue in topsort order
             # if there are fewer workers than nodes, and the queue is populated
             # in a non-topsort order, some worker may just wait forever for it's
@@ -208,74 +212,12 @@ class PyRuntime(RuntimeClient):
         # Wait until all worker tasks are cancelled.
         await asyncio.gather(*workers, return_exceptions=True)
 
-        return {
-            e.target.port: runtime_state.pop(e.to_edge_handle())
-            for e in run_g.in_edges(run_g.output_node_idx)
-        }
+        out_node = run_g.children(parent)[1]
+        return await get_inputs(out_node, wait=False) # No need to wait, all nodes finishing executing.
 
-    async def _run_eval(
-        self, ins: dict[str, TierkreisValue]
-    ) -> dict[str, TierkreisValue]:
-        thunk = cast(GraphValue, ins.pop(Labels.THUNK)).value
-        return await self.run_graph(thunk, **ins)
 
-    async def _run_loop(
-        self, ins: dict[str, TierkreisValue]
-    ) -> dict[str, TierkreisValue]:
-        body = cast(GraphValue, ins.pop("body")).value
-        while True:
-            outs = await self.run_graph(
-                body,
-                **ins,
-            )
-            out = cast(
-                VariantValue,
-                outs[Labels.VALUE],
-            )
-            nxt = {"value": out.value}
-            if out.tag == Labels.BREAK:
-                return nxt
-            else:
-                ins = nxt
-
-    async def _run_map(
-        self, ins: dict[str, TierkreisValue]
-    ) -> dict[str, TierkreisValue]:
-        body = cast(GraphValue, ins.pop("thunk")).value
-        inputs = cast(VecValue, ins.pop("value")).values
-
-        async def task(x):
-            return (await self.run_graph(body, value=x))[Labels.VALUE]
-
-        tasks = [asyncio.create_task(task(x)) for x in inputs]
-        out = await asyncio.gather(*tasks)
-        ret = {"value": cast(TierkreisValue, VecValue(out))}
-        return ret
-
-    def _run_match(self, ins: dict[str, TierkreisValue]) -> dict[str, TierkreisValue]:
-        variant = cast(VariantValue, ins[Labels.VARIANT_VALUE])
-        thunk = cast(GraphValue, ins[variant.tag]).value
-
-        newg = TierkreisGraph()
-        boxinps: dict[str, IncomingWireType] = {
-            inp: newg.input[inp] for inp in thunk.inputs()
-        }
-        boxinps[Labels.VALUE] = newg.add_const(variant.value)
-        box = newg.add_box(thunk, **boxinps)
-        newg.set_outputs(**{out: box[out] for out in thunk.outputs()})
-        return {Labels.THUNK: GraphValue(newg)}
-
-    async def get_signature(self) -> Signature:
-        return self.root.extract_signature(True)
-
-    async def type_check_graph(self, graph: TierkreisGraph) -> TierkreisGraph:
-        return infer_graph_types(graph, await self.get_signature())
-
-    async def type_check_graph_with_inputs(
-        self, tg, inputs: StructValue
-    ) -> Tuple[TierkreisGraph, StructValue]:
-        return infer_graph_types(tg, await self.get_signature(), inputs)
-
-    @property
-    def can_type_check(self) -> bool:
-        return _TYPE_CHECK
+def unpack_first(*vals: Value) -> tuple[int, list[Value]]:
+    pred = vals[0]
+    assert isinstance(pred, Sum)
+    pred.vals.extend(vals[1:])
+    return (pred.tag, pred.vals)

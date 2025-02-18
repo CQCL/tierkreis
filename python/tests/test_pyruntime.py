e7937b41b64a577800a9548d8144f733be5affd9
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2025-02-18 15:11:20 +0000

    Test: use Hugr.load_json

--- python/tests/test_pyruntime.py
+++ python/tests/test_pyruntime.py
@@ -1,10 +1,8 @@
 import pytest
-import json
 
 from tierkreis.pyruntime import PyRuntime
 
 from hugr import Hugr
-from hugr._serialization.serial_hugr import SerialHugr
 from hugr.val import TRUE, FALSE, Tuple
 from hugr.std.int import IntVal
 
@@ -12,7 +10,7 @@ from hugr.std.int import IntVal
 @pytest.mark.asyncio
 async def test_factorial():
     with open("/Users/alanlawrence/factorial_hugr.json") as f:
-        h = Hugr._from_serial(SerialHugr.load_json(json.loads(f.read())))
+        h = Hugr.load_json(f.read())
     outs = await PyRuntime().run_graph(h)
     assert outs == [IntVal(120, 5).to_value()]
 
@@ -20,6 +18,6 @@ async def test_factorial():
 @pytest.mark.asyncio
 async def test_fibonacci():
     with open("/Users/alanlawrence/fibonacci_hugr.json") as f:
-        h = Hugr._from_serial(SerialHugr.load_json(json.loads(f.read())))
+        h = Hugr.load_json(f.read())
     outs = await PyRuntime().run_graph(h)
     assert outs == [IntVal(8, 5).to_value()]

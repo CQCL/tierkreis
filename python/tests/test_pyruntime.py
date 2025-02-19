b305d9a64285dd1363625d1d03fced072253ab1a
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2025-02-19 12:48:14 +0000

    And another test, calls/tuples

--- python/tests/test_pyruntime.py
+++ python/tests/test_pyruntime.py
@@ -28,23 +28,28 @@ async def test_fibonacci():
     outs = await PyRuntime().run_graph(h)
     assert outs == [IntVal(8, 5).to_value()]
 
+
 @pytest.mark.asyncio
 async def test_double_type_change():
     module = GuppyModule("module")
+
     @guppy(module)
     def foo(b: bool) -> int:
         y = 4
         (y := 1) if (y := b) else (y := 6)
         return y
+
     (h,) = foo.compile().package.modules
     outs = await PyRuntime().run_graph(h, True)
     assert outs == [IntVal(1, width=6)]
     outs = await PyRuntime().run_graph(h, False)
     assert outs == [IntVal(6, width=6)]
 
+
 @pytest.mark.asyncio
 async def test_break_different_types1a():
     module = GuppyModule("module")
+
     @guppy(module)
     def foo(x: int) -> int:
         z = 0
@@ -56,8 +61,27 @@ async def test_break_different_types1a():
                 z = 8
             x += z
         return 3 if z else x
+
     (h,) = foo.compile().package.modules
     outs = await PyRuntime().run_graph(h, IntVal(3, 6))
     assert outs == [IntVal(11, width=6).to_value()]
     outs = await PyRuntime().run_graph(h, IntVal(6, 6))
     assert outs == [IntVal(6, width=6)]
+
+
+@pytest.mark.asyncio
+async def test_calls_tuples():
+    module = GuppyModule("module")
+
+    @guppy(module)
+    def foo(x: int) -> tuple[int]:
+        return (x,)
+
+    @guppy(module)
+    def main(x: int) -> int:
+        (y,) = foo(x)
+        return y
+
+    h = module.compile().module
+    outs = await PyRuntime().run_graph(h, IntVal(3, 6))
+    assert outs == [IntVal(3, 6)]

21dfaedde61284d30924053030413fcdb3b1da47
Matthew Burke <matthew.burke@quantinuum.com>
2025-03-20 11:11:24 +0000

    Revert test improvements

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -5,7 +5,7 @@ from typing import AsyncIterator
 import pytest
 from grpclib.client import Channel
 
-from tests.test_worker import main
+from test_worker import main
 from tierkreis.builder import Namespace
 from tierkreis.client.runtime_client import RuntimeClient
 from tierkreis.client.server_client import ServerRuntime

0c24bf55a29e2b4c9e86954570a8f0373f659bc8
Matthew Burke <matthew.burke@quantinuum.com>
2025-04-23 15:00:51 +0100

    Remove extra models

--- python/tierkreis/controller/data/location.py
+++ python/tierkreis/controller/data/location.py
@@ -22,51 +22,6 @@ class WorkerCallArgs(BaseModel):
     logs_path: Optional[Path]
 
 
-class NodeType(Enum):
-    NODE = 0
-    LOOP = 1
-    MAP = 2
-
-    def __str__(self) -> str:
-        match self:
-            case NodeType.NODE:
-                return "N"
-            case NodeType.LOOP:
-                return "L"
-            case NodeType.MAP:
-                return "M"
-            case _:
-                assert_never(self)
-
-    @staticmethod
-    def from_str(node_type: str) -> "NodeType":
-        match node_type:
-            case "N":
-                return NodeType.NODE
-            case "L":
-                return NodeType.LOOP
-            case "M":
-                return NodeType.MAP
-            case _:
-                raise TierkreisError("Unknown node type.")
-
-
-class NodeStep(BaseModel):
-    node_type: NodeType
-    idx: int
-
-    def __str__(self) -> str:
-        return f"{self.node_type}{self.idx}"
-
-    @staticmethod
-    def from_str(frame: str) -> "NodeStep":
-        try:
-            c, idx = frame[0], int(frame[1:])
-            return NodeStep(node_type=NodeType.from_str(c), idx=idx)
-        except (IndexError, ValueError) as exc:
-            raise TierkreisError(f"Invalid NodeStep {frame}") from exc
-
-
 class Loc(str):
     def __new__(cls, k: str = "-") -> "Loc":
         return super(Loc, cls).__new__(cls, k)

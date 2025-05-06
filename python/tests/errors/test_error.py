344900e6e3a1547eae410be72e5c77b3e3fef6be
Philipp Seitz <philipp.seitz@quantinuum.com>
2025-05-06 14:41:48 +0100

    Imporve testcase

--- python/tests/errors/test_error.py
+++ python/tests/errors/test_error.py
@@ -10,18 +10,30 @@ from tierkreis.controller.executor.uv_executor import UvExecutor
 from tierkreis.controller.storage.filestorage import ControllerFileStorage
 
 
-
 def will_fail() -> GraphData:
     graph = GraphData()
     graph.add(Func("failing_worker.fail", {}))("fail")
     return graph
 
+def wont_fail() -> GraphData:
+    graph = GraphData()
+    graph.add(Func("failing_worker.wont_fail", {}))("wont_fail")
+    return graph
 
-def test_raise_error():
+def test_raise_error() -> None:
     g = will_fail()
     storage = ControllerFileStorage(UUID(int=42), name="will_fail")
     executor = UvExecutor(Path("./python/tests/errors"), logs_path=storage.logs_path)
     storage.clean_graph_files()
     run_graph(storage, executor, g, {}, n_iterations=10)
     error_path = storage.logs_path.parent / "-.N0/errors"
-    assert error_path.exists()
+    assert storage.node_has_error("-.N0")
+    assert error_path.read_text().startswith("Traceback (most recent call last):")
+
+def test_raises_no_error() -> None:
+    g = wont_fail()
+    storage = ControllerFileStorage(UUID(int=43), name="wont_fail")
+    executor = UvExecutor(Path("./python/tests/errors"), logs_path=storage.logs_path)
+    storage.clean_graph_files()
+    run_graph(storage, executor, g, {}, n_iterations=10)
+    assert not storage.node_has_error("-.N0")

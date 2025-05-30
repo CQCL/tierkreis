cdfa89b01dc1533b37dd7c77255c04da7ab2a5d8
Philipp Seitz <philipp.seitz@quantinuum.com>
2025-05-30 13:31:22 +0100

    Add nodes to index

--- tierkreis_visualization/frontend/src/nodes/index.ts
+++ tierkreis_visualization/frontend/src/nodes/index.ts
@@ -2,7 +2,11 @@ import type { NodeTypes } from '@xyflow/react';
 
 import { ConstNode } from './const';
 import { EvalNode } from './eval';
-import { InputNode }   from './input';
+import { FunctionNode } from './function';
+import { InputNode } from './input';
+import { IfElseNode } from './ifelse';
+import { LoopNode } from './Loop';
+import { MapNode } from './map';
 import { OutputNode } from './output';
 import { AppNode } from './types';
 
@@ -13,4 +17,8 @@ export const nodeTypes = {
   "const-node": ConstNode,
   "output-node": OutputNode,
   "eval-node": EvalNode,
+  "function-node": FunctionNode,
+  "loop-node": LoopNode,
+  "map-node": MapNode,
+  "ifelse-node": IfElseNode,
 } satisfies NodeTypes;

df9fb5d18ecfd34a24767798ee259b02f7e587d6
Philipp Seitz <philipp.seitz@quantinuum.com>
2025-05-30 13:44:23 +0100

    Add logic to load child nodes in eval

--- tierkreis_visualization/frontend/src/nodes/types.ts
+++ tierkreis_visualization/frontend/src/nodes/types.ts
@@ -1,5 +1,25 @@
-import type { Node, BuiltInNode } from '@xyflow/react';
+import {
+    type Edge,
+    type Node,
+    type BuiltInNode,
+    type OnNodesChange,
+    type OnEdgesChange,
+    type OnConnect,
+} from '@xyflow/react';
 
-export type PyNode = { id: string | number ; status: "Not started" | "Started" | "Error" | "Finished"; function_name: string };
-export type BackendNode = Node<{ name: string, status: "Started" | "Finished" | "Error" | "Not Started", outputs: [{ name: string, value: any }] }, 'Input'>;
+
+export type PyNode = { id: string | number; status: "Not started" | "Started" | "Error" | "Finished"; function_name: string };
+export type BackendNode = Node<{ name: string, status: "Started" | "Finished" | "Error" | "Not Started", outputs: [{ name: string, value: any }], id: string }, 'Input'>;
 export type AppNode = BuiltInNode | BackendNode;
+
+export type AppState = {
+    nodes: AppNode[];
+    edges: Edge[];
+    onNodesChange: OnNodesChange<AppNode>;
+    onEdgesChange: OnEdgesChange;
+    onConnect: OnConnect;
+    setNodes: (nodes: AppNode[]) => void;
+    setEdges: (edges: Edge[]) => void;
+    appendNodes: (node: AppNode[]) => void;
+    appendEdges: (edge: Edge[]) => void;
+};

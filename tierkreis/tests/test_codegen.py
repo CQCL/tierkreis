50ff8ad23284f2e598597d3556692d8d734fe180
Matthew Burke <matthew.burke@quantinuum.com>
2025-06-03 11:23:39 +0100

    Remove codegen test

--- tierkreis/tests/test_codegen.py
+++ tierkreis/tests/test_codegen.py
@@ -6,11 +6,6 @@ from tierkreis.controller.builtins.main import worker
 logger = getLogger(__name__)
 
 
-def test_codegen():
-    print(format_namespace(worker.namespace))
-    assert False
-
-
 if __name__ == "__main__":
     with open(TESTS_PATH / "tkr_builtins.py", "w+") as fh:
         fh.write(format_namespace(worker.namespace))

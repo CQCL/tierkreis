ad40eebf953c0978355cd71f415e34466b7d5e24
Matthew Burke <matthew.burke@quantinuum.com>
2025-06-16 10:01:27 +0100

    Tighten up FunctionSpec methods

--- tierkreis/tierkreis/namespace.py
+++ tierkreis/tierkreis/namespace.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import inspect
 from logging import getLogger
+from types import NoneType
 from typing import Any, get_args
 from pydantic import BaseModel
 from tierkreis.controller.data.core import DictConvertible, PortID, TKType
@@ -20,25 +21,28 @@ class FunctionSpec:
     ins: dict[PortID, type[TKType] | str]
     outs: dict[PortID, type[TKType] | str]
 
-    def _parse_annotation(self, annotation: Any) -> type[TKType] | str:
+    def _parse_annotation(self, annotation: type | None) -> type[TKType] | str:
+        if annotation is None:
+            return NoneType
+
         if annotation in get_args(TKType):
             return annotation
 
-        if inspect.isclass(annotation) and issubclass(annotation, BaseModel):
-            return annotation.__name__
-
-        if inspect.isclass(annotation) and issubclass(annotation, DictConvertible):
-            return annotation.__name__
+        if inspect.isclass(annotation):
+            if issubclass(annotation, (BaseModel, DictConvertible)):
+                return annotation.__name__
 
         raise TierkreisWorkerError(f"Unsupported annotation {annotation}.")
 
     def add_inputs(self, annotations: dict[str, Any]) -> None:
         for name, annotation in annotations.items():
-            if name == "return":
-                continue
             self.ins[name] = self._parse_annotation(annotation)
 
-    def add_outputs(self, annotation: Any) -> None:
+    def add_outputs(self, annotation: type | None) -> None:
+        if annotation is None:
+            self.outs["value"] = NoneType
+            return
+
         self._parse_annotation(annotation)
         if issubclass(annotation, BaseModel):
             for field, info in annotation.model_fields.items():
@@ -55,7 +59,7 @@ class Namespace:
     def add_from_annotations(self, name: str, annotations: dict[str, Any]) -> None:
         try:
             fn = FunctionSpec(name=name, namespace=self.name, ins={}, outs={})
-            fn.add_inputs({k: v for k, v in annotations.items() if k != "result"})
+            fn.add_inputs({k: v for k, v in annotations.items() if k != "return"})
             fn.add_outputs(annotations["return"])
             self.functions.append(fn)
         except TierkreisWorkerError as exc:

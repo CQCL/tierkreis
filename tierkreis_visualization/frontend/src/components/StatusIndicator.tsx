549de5d8c8583fc73ded5e2aa1f468173bd6e122
Philipp Seitz <philipp.seitz@quantinuum.com>
2025-06-17 13:09:35 +0100

    Fix status indicator

--- tierkreis_visualization/frontend/src/components/StatusIndicator.tsx
+++ tierkreis_visualization/frontend/src/components/StatusIndicator.tsx
@@ -9,7 +9,7 @@ export type NodeStatusIndicatorProps = {
 export const LoadingIndicator = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <div className="absolute -left-[1px] -top-[1px] h-[calc(100%+2px)] w-[calc(100%+2px)]">
+      <div className="absolute -left-[1px] -top-[1px] h-[calc(100%+2px)] w-[calc(100%+2px)] ">
         <style>
           {`
         @keyframes spin {
@@ -24,10 +24,11 @@ export const LoadingIndicator = ({ children }: { children: ReactNode }) => {
           width: 140%;
           aspect-ratio: 1;
           transform-origin: center;
-        }
+          z-index: -1; // manual fix for the card
+          }
       `}
         </style>
-        <div className="absolute inset-0 overflow-hidden rounded-xl">
+        <div className="absolute inset-0 overflow-hidden rounded-xl ">
           <div className="spinner rounded-full bg-[conic-gradient(from_0deg_at_50%_50%,_rgb(42,67,233)_0deg,_rgba(42,138,246,0)_360deg)]" />
         </div>
       </div>

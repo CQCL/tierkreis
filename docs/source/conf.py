be59aa1d635767d461695f8c0489c91ae99b48a1
John Children <john.children@quantinuum.com>
2025-06-26 13:16:03 +0100

    docs: Add basic sphinx configuration

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'tierkreis'
+copyright = '2025, Quantinuum'
+author = 'Quantinuum'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['quantinuum_sphinx']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'quantinuum_sphinx'
+html_static_path = ['_static']
+html_favicon = '_static/quantinuum_favicon.svg'

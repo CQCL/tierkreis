0d49f7e906fd5c1743c55579f937bf5bf5269063
Philipp Seitz <philipp.seitz@quantinuum.com>
2025-06-27 16:32:50 +0100

    Replace re/undo with recalculate

--- tierkreis_visualization/frontend/src/App.tsx
+++ tierkreis_visualization/frontend/src/App.tsx
@@ -6,7 +6,7 @@ import {
   ReactFlowProvider,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
-import { RedoDot, UndoDot } from "lucide-react";
+import { RedoDot, UndoDot, Network } from "lucide-react";
 import { useShallow } from "zustand/react/shallow";
 
 import Layout from "@/components/layout";
@@ -15,6 +15,7 @@ import useStore from "@/data/store";
 import { edgeTypes } from "@/edges";
 import { nodeTypes } from "@/nodes";
 import { AppState } from "@/nodes/types";
+import { stat } from "fs";
 
 const selector = (state: AppState) => ({
   nodes: state.nodes,
@@ -22,12 +23,18 @@ const selector = (state: AppState) => ({
   onNodesChange: state.onNodesChange,
   onEdgesChange: state.onEdgesChange,
   onConnect: state.onConnect,
+  recalculateNodePositions: state.recalculateNodePositions,
 });
 
 export default function App() {
-  const { nodes, edges, onNodesChange, onEdgesChange, onConnect } = useStore(
-    useShallow(selector)
-  );
+  const {
+    nodes,
+    edges,
+    onNodesChange,
+    onEdgesChange,
+    onConnect,
+    recalculateNodePositions,
+  } = useStore(useShallow(selector));
   const { undo, redo } = useStore.temporal.getState();
   return (
     <Layout>
@@ -40,16 +47,21 @@ export default function App() {
           edgeTypes={edgeTypes}
           onEdgesChange={onEdgesChange}
           onConnect={onConnect}
+          // minZoom={0.001}
+          // maxZoom={10}
           fitView
         >
           <Background />
           <Controls showZoom={false} showInteractive={false}>
             <SidebarTrigger style={{ fill: "none" }} />
-            <ControlButton onClick={() => undo(2)}>
+            {/* <ControlButton onClick={() => undo(2)}>
               <UndoDot style={{ fill: "none" }} />
             </ControlButton>
             <ControlButton onClick={() => redo(2)}>
               <RedoDot style={{ fill: "none" }} />
+            </ControlButton> */}
+            <ControlButton onClick={() => recalculateNodePositions()}>
+              <Network style={{ fill: "none" }} />
             </ControlButton>
           </Controls>
         </ReactFlow>

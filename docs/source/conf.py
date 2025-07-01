8cfa97030dd49b56dfa54b51cbbb3fb98b632480
John Children <john.children@quantinuum.com>
2025-07-01 17:12:14 +0100

    feat: Add automatic API documentation
    Adds API documentation for everything, a little bit overkill but we
    can remove anything that is overly verbose by going through and
    setting __all__ attributes where necessary.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,17 @@ author = "Quantinuum"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["quantinuum_sphinx", "myst_parser"]
+extensions = [
+    "autodoc2",
+    "myst_parser",
+    "quantinuum_sphinx",
+]
+autodoc2_packages = [
+    "../../tierkreis/tierkreis",
+    "../../tierkreis_workers/aer_worker",
+    "../../tierkreis_workers/nexus_worker",
+    "../../tierkreis_workers/pytket_worker",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []

624cf5041e7bd73f9f9f4448bf359fd1b54f0043
Philipp Seitz <philipp.seitz@quantinuum.com>
2025-07-22 16:01:02 +0100

    Add symbolic visualization

--- tierkreis/tierkreis/utils.py
+++ tierkreis/tierkreis/utils.py
@@ -59,3 +59,40 @@ def visualize_graph(
         finally:
             logger.info("\nBrowser closed! Resuming Python script.")
             server_thread.join(timeout=5)
+
+
+@patch("tierkreis_visualization.routers.workflows.get_storage")
+@patch("tierkreis_visualization.routers.workflows.get_workflows")
+def visualize_graph_symbolic(
+    graph: GraphData,
+    get_workflows: MagicMock,
+    get_storage: MagicMock,
+    default_browser: Literal["chromium", "webkit", "firefox"] = "chromium",
+) -> None:
+    storage = ControllerInMemoryStorage(UUID(int=0), name="tmp")
+    storage.evaluate_symbolic(graph)
+    get_workflows.return_value = [WorkflowDisplay(id=UUID(int=0), id_int=0, name="tmp")]
+    get_storage.return_value = storage
+
+    server_thread = threading.Thread(target=start, daemon=True)
+    server_thread.start()
+    with sync_playwright() as plw:
+        try:
+            browser_type = {
+                "chromium": plw.chromium,
+                "firefox": plw.firefox,
+                "webkit": plw.webkit,
+            }
+            browser = browser_type[default_browser].launch(headless=False)
+            page = browser.new_page()
+            page.goto("http://127.0.0.1:8000")
+            page.evaluate("() => localStorage.clear()")
+            page.reload()
+
+            page.wait_for_event("close")
+
+        except Error as e:
+            logger.error("Browser was closed prematurely: %s", e)
+        finally:
+            logger.info("\nBrowser closed! Resuming Python script.")
+            server_thread.join(timeout=5)

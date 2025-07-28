54059e49733900b781468502a6a86fc64c5683c5
Philipp Seitz <philipp.seitz@quantinuum.com>
2025-07-28 10:19:20 +0100

    Simplify visualization utils

--- tierkreis/tierkreis/utils.py
+++ tierkreis/tierkreis/utils.py
@@ -1,15 +1,12 @@
-from pathlib import Path
 from uuid import UUID
-from typing import Any, Literal
+from typing import Literal
 from unittest.mock import MagicMock, patch
 import threading
 import logging
 
 from playwright.sync_api import sync_playwright, Error
 
-from tierkreis.controller import run_graph
 from tierkreis.controller.data.graph import GraphData
-from tierkreis.controller.executor.in_memory_executor import InMemoryExecutor
 from tierkreis.controller.storage.in_memory import ControllerInMemoryStorage
 from tierkreis_visualization.data.workflows import WorkflowDisplay
 from tierkreis_visualization.main import start
@@ -21,54 +18,21 @@ logger = logging.getLogger(__name__)
 @patch("tierkreis_visualization.routers.workflows.get_workflows")
 def visualize_graph(
     graph: GraphData,
-    inputs: dict[str, Any],
     get_workflows: MagicMock,
     get_storage: MagicMock,
     default_browser: Literal["chromium", "webkit", "firefox"] = "chromium",
 ) -> None:
-    storage = ControllerInMemoryStorage(UUID(int=0), name="tmp")
-    executor = InMemoryExecutor(
-        registry_path=Path("tierkreis/tierkreis"), storage=storage
-    )
-    run_graph(
-        storage, executor, graph, inputs, n_iterations=50, polling_interval_seconds=0
-    )
+    """Visualize a computation graph in a web browser.
 
-    get_workflows.return_value = [WorkflowDisplay(id=UUID(int=0), id_int=0, name="tmp")]
-    get_storage.return_value = storage
-
-    server_thread = threading.Thread(target=start, daemon=True)
-    server_thread.start()
-    with sync_playwright() as plw:
-        try:
-            browser_type = {
-                "chromium": plw.chromium,
-                "firefox": plw.firefox,
-                "webkit": plw.webkit,
-            }
-            browser = browser_type[default_browser].launch(headless=False)
-            page = browser.new_page()
-            page.goto("http://127.0.0.1:8000")
-            page.evaluate("() => localStorage.clear()")
-            page.reload()
-
-            page.wait_for_event("close")
-
-        except Error as e:
-            logger.error("Browser was closed prematurely: %s", e)
-        finally:
-            logger.info("\nBrowser closed! Resuming Python script.")
-            server_thread.join(timeout=5)
-
-
-@patch("tierkreis_visualization.routers.workflows.get_storage")
-@patch("tierkreis_visualization.routers.workflows.get_workflows")
-def visualize_graph_symbolic(
-    graph: GraphData,
-    get_workflows: MagicMock,
-    get_storage: MagicMock,
-    default_browser: Literal["chromium", "webkit", "firefox"] = "chromium",
-) -> None:
+    :param graph: The computation graph to visualize.
+    :type graph: GraphData
+    :param get_workflows: Internal; Used to inject the workflow retrieval mock.
+    :type get_workflows: MagicMock
+    :param get_storage: Internal; Used to inject the storage mock.
+    :type get_storage: MagicMock
+    :param default_browser: The default browser to use, defaults to "chromium"
+    :type default_browser: Literal[&quot;chromium&quot;, &quot;webkit&quot;, &quot;firefox&quot;], optional
+    """
     storage = ControllerInMemoryStorage(UUID(int=0), name="tmp")
     storage.evaluate_symbolic(graph)
     get_workflows.return_value = [WorkflowDisplay(id=UUID(int=0), id_int=0, name="tmp")]

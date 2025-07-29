b94d4a28a3716b3456a654bf90baa3e06ba784bb
Matthew Burke <matthew.burke@quantinuum.com>
2025-07-29 12:07:13 +0100

    Fix reexports

--- tierkreis/tierkreis/__init__.py
+++ tierkreis/tierkreis/__init__.py
@@ -3,4 +3,4 @@ from tierkreis.worker.worker import Worker
 from tierkreis.value import Value
 from tierkreis.controller import run_graph
 
-__all__ = ["Labels", "Worker", "Value"]
+__all__ = ["Labels", "Worker", "Value", "run_graph"]
--- tierkreis/tierkreis/storage.py
+++ tierkreis/tierkreis/storage.py
@@ -1,12 +1,14 @@
 from tierkreis.controller.data.location import Loc
-from tierkreis.controller.data.types import ptype_from_bytes
+from tierkreis.controller.data.types import PType, ptype_from_bytes
 from tierkreis.controller.storage.protocol import ControllerStorage
 from tierkreis.controller.storage.filestorage import (
     ControllerFileStorage as FileStorage,
 )
 
+__all__ = ["FileStorage"]
 
-def read_outputs(storage: ControllerStorage) -> dict[str, bytes] | bytes:
+
+def read_outputs(storage: ControllerStorage) -> dict[str, PType] | PType:
     out_ports = storage.read_output_ports(Loc())
     if len(out_ports) == 1 and "value" in out_ports:
         return ptype_from_bytes(storage.read_output(Loc(), "value"))

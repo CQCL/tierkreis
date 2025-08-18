6d2071a218935c6d8859f84e4027dc9fb490186c
Philipp Seitz <philipp.seitz@quantinuum.com>
2025-08-18 13:08:47 +0100

    Add more predefined classes

--- tierkreis/tierkreis/controller/executor/hpc/adapter.py
+++ tierkreis/tierkreis/controller/executor/hpc/adapter.py
@@ -1,3 +1,10 @@
+from functools import partial
+
+from tierkreis.controller.executor.hpc.hpc_executor import HpcExecutor
+from tierkreis.controller.executor.hpc.job_spec import (
+    pjsub_large_spec,
+    pjsub_small_spec,
+)
 from tierkreis.controller.executor.hpc.protocol import TemplateAdapter
 
 _template_dir = "./tierkreis/tierkreis/controller/executor/hpc"
@@ -5,3 +12,12 @@ _template_dir = "./tierkreis/tierkreis/controller/executor/hpc"
 PJSUB_ADAPTER = TemplateAdapter("pjsub.jinja", "pjsub", template_dir=_template_dir)
 PBS_ADAPTER = TemplateAdapter("pbs.jinja", "qsub", template_dir=_template_dir)
 SLURM_ADAPTER = TemplateAdapter("slurm.jinja", "sbatch", template_dir=_template_dir)
+
+PJSUB_EXECUTOR_SMALL = partial(
+    HpcExecutor, adapter=PJSUB_ADAPTER, spec=pjsub_small_spec()
+)
+PJSUB_EXECUTOR_LARGE = partial(
+    HpcExecutor, adapter=PJSUB_ADAPTER, spec=pjsub_large_spec()
+)
+PBS_EXECUTOR = partial(HpcExecutor, adapter=PBS_ADAPTER)
+SLURM_EXECUTOR = partial(HpcExecutor, adapter=SLURM_ADAPTER)

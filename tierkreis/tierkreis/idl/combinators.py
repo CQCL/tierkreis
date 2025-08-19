f558f229fe9d73ab82cea2c955173af013f30dbb
Matthew Burke <matthew.burke@quantinuum.com>
2025-08-19 15:03:42 +0100

    Clean up

--- tierkreis/tierkreis/idl/combinators.py
+++ tierkreis/tierkreis/idl/combinators.py
@@ -1,138 +1,10 @@
-import re
 from types import NoneType
-from typing import Callable, NamedTuple, overload
+from typing import NamedTuple
 
 from tierkreis.controller.data.models import TKR_PORTMAPPING_FLAG
 from tierkreis.exceptions import TierkreisError
+from tierkreis.idl.parser import Parser, lit, reg, seq
 from tierkreis.namespace import FunctionSpec, Namespace
-
-
-class ParserError(Exception): ...
-
-
-class Parser[T]:
-    def __init__(self, fn: Callable[[str], tuple[T, str]]) -> NoneType:
-        def f(ins: str):
-            ins = ins.strip()
-            return fn(ins)
-
-        self.fn = f
-
-    def __call__(self, ins: str) -> tuple[T, str]:
-        return self.fn(ins)
-
-    def __or__[S](
-        self, other: "Parser[S]" | Callable[[str], tuple[S, str]]
-    ) -> "Parser[T|S]":
-        def f(ins: str):
-            try:
-                return self(ins)
-            except:
-                return other(ins)
-
-        return Parser(f)
-
-    def __and__[S](self, other: "Parser[S]") -> "Parser[tuple[T,S]]":
-        def f(ins: str):
-            s, remaining = self(ins)
-            t, remaining = other(remaining)
-            return (s, t), remaining
-
-        return Parser(f)
-
-    def __lshift__[S](self, other: "Parser[S]") -> "Parser[T]":
-        def f(ins: str):
-            t, remaining = self(ins)
-            _, remaining = other(remaining)
-            return t, remaining
-
-        return Parser(f)
-
-    def __rshift__[S](self, other: "Parser[S]") -> "Parser[S]":
-        def f(ins: str):
-            _, remaining = self(ins)
-            s, remaining = other(remaining)
-            return s, remaining
-
-        return Parser(f)
-
-    def map[A](self, fn: Callable[[T], A]) -> "Parser[A]":
-        def f(ins: str):
-            t, remaining = self(ins)
-            return fn(t), remaining
-
-        return Parser(f)
-
-    def coerce[A](self, a: A) -> "Parser[A]":
-        def f(ins: str):
-            t, remaining = self(ins)
-            return a, remaining
-
-        return Parser(f)
-
-    def rep(self, sep: "Parser[str] | None" = None) -> "Parser[list[T]]":
-        def f(ins: str):
-            outs: list[T] = []
-            while True:
-                try:
-                    t, ins = self(ins)
-                    if sep:
-                        try:
-                            _, ins = sep(ins)
-                        except ParserError:
-                            pass
-                    outs.append(t)
-                except ParserError:
-                    break
-            return outs, ins
-
-        return Parser(f)
-
-
-@overload
-def seq[A, B](*args: *tuple[Parser[A], Parser[B]]) -> Parser[tuple[A, B]]: ...
-@overload
-def seq[A, B, C](
-    *args: *tuple[Parser[A], Parser[B], Parser[C]],
-) -> Parser[tuple[A, B, C]]: ...
-@overload
-def seq[A, B, C, D](
-    *args: *tuple[Parser[A], Parser[B], Parser[C], Parser[D]],
-) -> Parser[tuple[A, B, C, D]]: ...
-def seq(*args: Parser) -> Parser[tuple]:
-    def f(ins: str):
-        outs = []
-        for arg in args:
-            s, ins = arg(ins)
-            outs.append(s)
-        return tuple(outs), ins
-
-    return Parser(f)
-
-
-# TODO: use typevar tuple here
-def lit(*args: str) -> Parser[str]:
-    def f(ins: str):
-        for a in args:
-            if ins.startswith(a):
-                return a, ins[len(a) :]
-
-        raise ParserError(f"lit: expected {args} found '{ins[:20]}'")
-
-    return Parser(f)
-
-
-def reg(regex: str) -> Parser[str]:
-    def f(ins: str):
-        r = re.compile("^(" + regex + ")")
-
-        if a := r.match(ins):
-            return a.group(0), ins[a.end() :]
-
-        raise ParserError(f"reg: expected regex {regex} found '{ins[:20]}'")
-
-    return Parser(f)
-
 
 signed_int = lit("integer", "int64", "int32", "int16", "int8", "safeint")
 unsigned_int = lit("uint64", "uint32", "uint16", "uint8")
@@ -142,14 +14,13 @@ bytes_t = lit("bytes").coerce(bytes)
 string_t = lit("string", "url").coerce(str)
 bool_t = lit("bool").coerce(bool)
 none_t = lit("null").coerce(NoneType)
-
-# decimal_t = lit("decimal", "decimal128") > fail("Decimal not implemented.")
-# date_t = lit(
-#     "plainDate", "plainTime", "utcDateTime", "offsetDateTime", "duration"
-# ) > fail("Date not implemented.")
-# unknown_t = lit("unknown", "void", "never") > (TierkreisError)
-
+decimal_t = lit("decimal", "decimal128").fail("Decimal")
+plain_datetime = lit("plainDate", "plainTime")
+other_date = lit("utcDateTime", "offsetDateTime", "duration")
+date_t = (plain_datetime | other_date).fail("Date")
+unknown_t = lit("unknown", "void", "never").fail("Unknown")
 identifier = reg(r"[a-zA-Z0-9_]*")
+
 type TypeTInner = int | float | bytes | bool | NoneType | str | list[TypeTInner] | dict[
     str, TypeTInner
 ]
@@ -178,7 +49,7 @@ def type_t_inner(ins: str) -> tuple[TypeT, str]:
     )(ins)
 
 
-type_t: Parser[TypeT] = Parser(type_t_inner)
+type_t = Parser[TypeT](type_t_inner)
 
 
 class TypeDecl(NamedTuple):
@@ -189,8 +60,8 @@ class TypeDecl(NamedTuple):
 type_decl = ((identifier << lit(":")) & type_t).map(lambda x: TypeDecl(*x))
 
 
-def parse_model(args: tuple[tuple[str, str], list[TypeDecl]]) -> type[NamedTuple]:
-    (id, name), decls = args
+def parse_model(args: tuple[str, str, list[TypeDecl]]) -> type[NamedTuple]:
+    id, name, decls = args
     a = [(x.name, x.t) for x in decls]
     nt = NamedTuple(name, a)
     if id == "portmapping":
@@ -198,10 +69,10 @@ def parse_model(args: tuple[tuple[str, str], list[TypeDecl]]) -> type[NamedTuple
     return nt
 
 
-model = (
-    lit("portmapping", "struct")
-    & identifier << lit("{")
-    & type_decl.rep(lit(";")) << lit("}")
+model = seq(
+    lit("portmapping", "struct"),
+    identifier << lit("{"),
+    type_decl.rep(lit(";")) << lit("}"),
 ).map(parse_model)
 
 
---
+++ tierkreis/tierkreis/idl/parser.py
@@ -0,0 +1,137 @@
+import re
+from types import NoneType
+from typing import Callable, Never, overload
+
+
+class ParserError(Exception): ...
+
+
+class Parser[T]:
+    fn: Callable[[str], tuple[T, str]]
+
+    def __init__(self, fn: Callable[[str], tuple[T, str]]) -> NoneType:
+        def f(ins: str):
+            ins = ins.strip()
+            return fn(ins)
+
+        self.fn = f
+
+    def __call__(self, ins: str) -> tuple[T, str]:
+        return self.fn(ins)
+
+    def __or__[S](
+        self, other: "Parser[S]" | Callable[[str], tuple[S, str]]
+    ) -> "Parser[T|S]":
+        def f(ins: str):
+            try:
+                return self(ins)
+            except:
+                return other(ins)
+
+        return Parser(f)
+
+    def __and__[S](self, other: "Parser[S]") -> "Parser[tuple[T,S]]":
+        def f(ins: str):
+            s, remaining = self(ins)
+            t, remaining = other(remaining)
+            return (s, t), remaining
+
+        return Parser(f)
+
+    def __lshift__[S](self, other: "Parser[S]") -> "Parser[T]":
+        def f(ins: str):
+            t, remaining = self(ins)
+            _, remaining = other(remaining)
+            return t, remaining
+
+        return Parser(f)
+
+    def __rshift__[S](self, other: "Parser[S]") -> "Parser[S]":
+        def f(ins: str):
+            _, remaining = self(ins)
+            s, remaining = other(remaining)
+            return s, remaining
+
+        return Parser(f)
+
+    def map[A](self, fn: Callable[[T], A]) -> "Parser[A]":
+        def f(ins: str):
+            t, remaining = self(ins)
+            return fn(t), remaining
+
+        return Parser(f)
+
+    def coerce[A](self, a: A) -> "Parser[A]":
+        def f(ins: str):
+            t, remaining = self(ins)
+            return a, remaining
+
+        return Parser(f)
+
+    def rep(self, sep: "Parser[str] | None" = None) -> "Parser[list[T]]":
+        def f(ins: str):
+            outs: list[T] = []
+            while True:
+                try:
+                    t, ins = self(ins)
+                    if sep:
+                        try:
+                            _, ins = sep(ins)
+                        except ParserError:
+                            pass
+                    outs.append(t)
+                except ParserError:
+                    break
+            return outs, ins
+
+        return Parser(f)
+
+    def fail(self, entity: str) -> "Parser[Never]":
+        def f(ins: str):
+            raise ParserError(f"{entity} not supported.")
+
+        return Parser(f)
+
+
+@overload
+def seq[A, B](*args: *tuple[Parser[A], Parser[B]]) -> Parser[tuple[A, B]]: ...
+@overload
+def seq[A, B, C](
+    *args: *tuple[Parser[A], Parser[B], Parser[C]],
+) -> Parser[tuple[A, B, C]]: ...
+@overload
+def seq[A, B, C, D](
+    *args: *tuple[Parser[A], Parser[B], Parser[C], Parser[D]],
+) -> Parser[tuple[A, B, C, D]]: ...
+def seq(*args: Parser) -> Parser[tuple]:
+    def f(ins: str):
+        outs = []
+        for arg in args:
+            s, ins = arg(ins)
+            outs.append(s)
+        return tuple(outs), ins
+
+    return Parser(f)
+
+
+def lit(*args: str) -> Parser[str]:
+    def f(ins: str):
+        for a in args:
+            if ins.startswith(a):
+                return a, ins[len(a) :]
+
+        raise ParserError(f"lit: expected {args} found '{ins[:20]}'")
+
+    return Parser(f)
+
+
+def reg(regex: str) -> Parser[str]:
+    def f(ins: str):
+        r = re.compile("^(" + regex + ")")
+
+        if a := r.match(ins):
+            return a.group(0), ins[a.end() :]
+
+        raise ParserError(f"reg: expected regex {regex} found '{ins[:20]}'")
+
+    return Parser(f)

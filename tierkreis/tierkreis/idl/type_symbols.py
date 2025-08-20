3945bd66765919f0c4e54afdc5ba92db8bc4e4ee
Matthew Burke <matthew.burke@quantinuum.com>
2025-08-20 15:48:18 +0100

    Fix identifier regex

--- tierkreis/tierkreis/idl/type_symbols.py
+++ tierkreis/tierkreis/idl/type_symbols.py
@@ -25,7 +25,7 @@ plain_datetime = lit("plainDate", "plainTime")
 other_date = lit("utcDateTime", "offsetDateTime", "duration")
 date_t = (plain_datetime | other_date).fail("Date")
 unknown_t = lit("unknown", "void", "never").fail("Unknown")
-identifier = reg(r"[a-zA-Z0-9_]*")
+identifier = reg(r"[a-zA-Z0-9_]+")
 forward_ref = identifier.map(ForwardRef)
 
 

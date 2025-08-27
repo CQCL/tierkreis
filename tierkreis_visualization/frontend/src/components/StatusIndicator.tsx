bc47e2a6e21592f6d6167dfb74064a3f911d5840
Philipp Seitz <philipp.seitz@quantinuum.com>
2025-08-27 11:54:15 +0100

    Change color

--- tierkreis_visualization/frontend/src/components/StatusIndicator.tsx
+++ tierkreis_visualization/frontend/src/components/StatusIndicator.tsx
@@ -9,7 +9,10 @@ export type NodeStatusIndicatorProps = {
 export const LoadingIndicator = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <div className="absolute -left-[1px] -top-[1px] h-[calc(100%+2px)] w-[calc(100%+2px)]" style={{ zIndex: "-1" }}>
+      <div
+        className="absolute -left-[1px] -top-[1px] h-[calc(100%+2px)] w-[calc(100%+2px)]"
+        style={{ zIndex: "-1" }}
+      >
         <style>
           {`
         @keyframes spin {
@@ -28,7 +31,10 @@ export const LoadingIndicator = ({ children }: { children: ReactNode }) => {
           }
       `}
         </style>
-        <div className="absolute inset-0 overflow-hidden rounded-xl" style={{ zIndex: "-1" }}>
+        <div
+          className="absolute inset-0 overflow-hidden rounded-xl"
+          style={{ zIndex: "-1" }}
+        >
           <div className="spinner rounded-full bg-[conic-gradient(from_0deg_at_50%_50%,_rgb(42,67,233)_0deg,_rgba(42,138,246,0)_360deg)]" />
         </div>
       </div>
@@ -64,7 +70,7 @@ export const NodeStatusIndicator = ({
 }: NodeStatusIndicatorProps) => {
   switch (status) {
     case "Started":
-      return <LoadingIndicator>{children}</LoadingIndicator>;
+      return <StatusBorder className="border-chart-4">{children}</StatusBorder>;
     case "Finished":
       return (
         <StatusBorder className="border-emerald-600">{children}</StatusBorder>

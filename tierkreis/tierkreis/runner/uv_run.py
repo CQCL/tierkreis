0c92183b27bb1ee07e57e916d15ce5a00283b747
Matthew Burke <matthew.burke@quantinuum.com>
2025-09-05 13:20:51 +0100

    Test classes

---
+++ tierkreis/tierkreis/runner/uv_run.py
@@ -0,0 +1,23 @@
+class UvRun:
+    def __init__(self, uv_project_directory: str) -> None:
+        self.project_dir = uv_project_directory
+
+    def __call__(self, inner_command: str) -> str:
+        return f"uv run --project {self.project_dir} {inner_command}"
+
+
+class StdOutIn:
+    def __init__(self, input_file: str, output_file: str) -> None:
+        self.input = input_file
+        self.output = output_file
+
+    def __call__(self, inner_command: str) -> str:
+        return f"{inner_command} < {self.input} > {self.output}"
+
+
+class DockerRun:
+    def __init__(self, container_name: str) -> None:
+        self.container = container_name
+
+    def __call__(self, inner_command: str) -> str:
+        return f'docker run {self.container} bash -c "{inner_command}"'

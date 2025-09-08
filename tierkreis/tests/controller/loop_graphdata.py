37cf54744163f496c6bcd05470f97e4e123eea2c
John Children <32305209+johnchildren@users.noreply.github.com>
2025-09-08 10:33:43 +0100

    Use newer syntax internally (#177)

--- tierkreis/tests/controller/loop_graphdata.py
+++ tierkreis/tests/controller/loop_graphdata.py
@@ -1,52 +1,118 @@
-from tierkreis.controller.data.graph import Const, Func, GraphData, Input, Loop, Output
-from tierkreis import Labels
+from typing import NamedTuple
+from tierkreis.builder import GraphBuilder
+from tierkreis.controller.data.core import EmptyModel
+from tierkreis.controller.data.graph import GraphData
+from tierkreis.controller.data.models import TKR
+from tierkreis.builtins.stubs import igt, iadd
 
 
-def _loop_body_multiple_acc() -> GraphData:
+def _loop_body_multiple_acc_untyped() -> GraphData:
     g = GraphData()
 
-    acc = g.add(Input("acc"))("acc")
-    acc2 = g.add(Input("acc2"))("acc2")
-    acc3 = g.add(Input("acc3"))("acc3")
+    acc = g.input("acc1")
+    acc2 = g.input("acc2")
+    acc3 = g.input("acc3")
 
-    one = g.add(Const(1))(Labels.VALUE)
-    two = g.add(Const(2))(Labels.VALUE)
-    three = g.add(Const(3))(Labels.VALUE)
-    five = g.add(Const(5))(Labels.VALUE)
+    one = g.const(1)
+    two = g.const(2)
+    three = g.const(3)
+    five = g.const(5)
 
-    should_continue = g.add(Func("builtins.igt", {"a": five, "b": acc}))(Labels.VALUE)
+    should_continue = g.func("builtins.igt", {"a": five, "b": acc})("value")
 
-    new_acc = g.add(Func("builtins.iadd", {"a": acc, "b": one}))(Labels.VALUE)
-    new_acc2 = g.add(Func("builtins.iadd", {"a": acc2, "b": two}))(Labels.VALUE)
-    new_acc3 = g.add(Func("builtins.iadd", {"a": acc3, "b": three}))(Labels.VALUE)
+    new_acc = g.func("builtins.iadd", {"a": acc, "b": one})("value")
+    new_acc2 = g.func("builtins.iadd", {"a": acc2, "b": two})("value")
+    new_acc3 = g.func("builtins.iadd", {"a": acc3, "b": three})("value")
 
-    g.add(
-        Output(
-            {
-                "should_continue": should_continue,
-                "acc": new_acc,
-                "acc2": new_acc2,
-                "acc3": new_acc3,
-            }
-        )
+    g.output(
+        {
+            "should_continue": should_continue,
+            "acc1": new_acc,
+            "acc2": new_acc2,
+            "acc3": new_acc3,
+        }
     )
 
     return g
 
 
-def loop_multiple_acc() -> GraphData:
+def loop_multiple_acc_untyped() -> GraphData:
     g = GraphData()
 
-    acc = g.add(Const(0))(Labels.VALUE)
-    acc2 = g.add(Const(0))(Labels.VALUE)
-    acc3 = g.add(Const(0))(Labels.VALUE)
+    acc1 = g.const(0)
+    acc2 = g.const(0)
+    acc3 = g.const(0)
 
-    body_const = g.add(Const(_loop_body_multiple_acc()))(Labels.VALUE)
+    body_const = g.const(_loop_body_multiple_acc_untyped())
 
-    loop = g.add(
-        Loop(body_const, {"acc": acc, "acc2": acc2, "acc3": acc3}, "should_continue")
+    loop = g.loop(
+        body_const, {"acc1": acc1, "acc2": acc2, "acc3": acc3}, "should_continue"
     )
 
-    g.add(Output({"acc": loop("acc"), "acc2": loop("acc2"), "acc3": loop("acc3")}))
+    g.output({"acc1": loop("acc1"), "acc2": loop("acc2"), "acc3": loop("acc3")})
+
+    return g
+
+
+class MultipleAcc(NamedTuple):
+    acc1: TKR[int]
+    acc2: TKR[int]
+    acc3: TKR[int]
+
+
+class MultipleAccOut(NamedTuple):
+    should_continue: TKR[bool]
+    acc1: TKR[int]
+    acc2: TKR[int]
+    acc3: TKR[int]
+
+
+def _loop_body_multiple_acc() -> GraphBuilder[MultipleAcc, MultipleAccOut]:
+    g = GraphBuilder(MultipleAcc, MultipleAccOut)
+
+    acc = g.inputs.acc1
+    acc2 = g.inputs.acc2
+    acc3 = g.inputs.acc3
+
+    one = g.const(1)
+    two = g.const(2)
+    three = g.const(3)
+    five = g.const(5)
+
+    should_continue = g.task(igt(a=five, b=acc))
+
+    new_acc = g.task(iadd(a=acc, b=one))
+    new_acc2 = g.task(iadd(a=acc2, b=two))
+    new_acc3 = g.task(iadd(a=acc3, b=three))
+
+    g.outputs(
+        MultipleAccOut(
+            should_continue=should_continue,
+            acc1=new_acc,
+            acc2=new_acc2,
+            acc3=new_acc3,
+        ),
+    )
+
+    return g
+
+
+class LoopMultipleAccOut(NamedTuple):
+    acc1: TKR[int]
+    acc2: TKR[int]
+    acc3: TKR[int]
+
+
+def loop_multiple_acc() -> GraphBuilder[EmptyModel, LoopMultipleAccOut]:
+    g = GraphBuilder(EmptyModel, LoopMultipleAccOut)
+
+    acc1 = g.const(0)
+    acc2 = g.const(0)
+    acc3 = g.const(0)
+
+    body = _loop_body_multiple_acc()
+    loop = g.loop(body, MultipleAcc(acc1, acc2, acc3))
+
+    g.outputs(LoopMultipleAccOut(acc1=loop.acc1, acc2=loop.acc2, acc3=loop.acc3))
 
     return g
--- tierkreis/tests/controller/sample_graphdata.py
+++ tierkreis/tests/controller/sample_graphdata.py
@@ -1,151 +1,137 @@
-from tierkreis.controller.data.graph import (
-    Const,
-    EagerIfElse,
-    Eval,
-    Func,
-    GraphData,
-    IfElse,
-    Input,
-    Loop,
-    Map,
-    Output,
-)
 from tierkreis import Labels
+from tierkreis.controller.data.graph import GraphData
 
 
 def doubler_plus() -> GraphData:
     g = GraphData()
-    inp = g.add(Input("doubler_input"))("doubler_input")
-    intercept = g.add(Input("intercept"))("intercept")
-    two = g.add(Const(2))(Labels.VALUE)
-    mul = g.add(Func("builtins.itimes", {"a": inp, "b": two}))(Labels.VALUE)
-    out = g.add(Func("builtins.iadd", {"a": mul, "b": intercept}))(Labels.VALUE)
-    g.add(Output({"doubler_output": out}))
+    inp = g.input("doubler_input")
+    intercept = g.input("intercept")
+    two = g.const(2)
+    mul = g.func("builtins.itimes", {"a": inp, "b": two})("value")
+    out = g.func("builtins.iadd", {"a": mul, "b": intercept})("value")
+    g.output({"doubler_output": out})
     return g
 
 
 def simple_eval() -> GraphData:
     g = GraphData()
-    zero = g.add(Const(0))(Labels.VALUE)
-    six = g.add(Const(6))(Labels.VALUE)
-    doubler_const = g.add(Const(doubler_plus()))(Labels.VALUE)
-    e = g.add(Eval(doubler_const, {"doubler_input": six, "intercept": zero}))
-    g.add(Output({"simple_eval_output": e("doubler_output")}))
+    zero = g.const(0)
+    six = g.const(6)
+    doubler_const = g.const(doubler_plus())
+    e = g.eval(doubler_const, {"doubler_input": six, "intercept": zero})
+    g.output({"simple_eval_output": e("doubler_output")})
     return g
 
 
 def simple_partial() -> GraphData:
     g = GraphData()
-    zero = g.add(Const(0))(Labels.VALUE)
-    six = g.add(Const(6))(Labels.VALUE)
-    doubler_const = g.add(Const(doubler_plus()))(Labels.VALUE)
-    partial = g.add(Eval(doubler_const, {"intercept": zero}))("body")
-    e = g.add(Eval(partial, {"doubler_input": six}))
-    g.add(Output({"simple_partial_output": e("doubler_output")}))
+    zero = g.const(0)
+    six = g.const(6)
+    doubler_const = g.const(doubler_plus())
+    partial = g.eval(doubler_const, {"intercept": zero})("body")
+    e = g.eval(partial, {"doubler_input": six})
+    g.output({"simple_partial_output": e("doubler_output")})
     return g
 
 
 def loop_body() -> GraphData:
     g = GraphData()
-    a = g.add(Input("loop_acc"))("loop_acc")
-    one = g.add(Const(1))(Labels.VALUE)
-    N = g.add(Const(10))(Labels.VALUE)
+    a = g.input("loop_acc")
+    one = g.const(1)
+    N = g.const(10)
 
-    a_plus = g.add(Func("builtins.iadd", {"a": a, "b": one}))(Labels.VALUE)
-    pred = g.add(Func("builtins.igt", {"a": N, "b": a_plus}))(Labels.VALUE)
-    g.add(Output({"loop_acc": a_plus, "should_continue": pred}))
+    a_plus = g.func("builtins.iadd", {"a": a, "b": one})("value")
+    pred = g.func("builtins.igt", {"a": N, "b": a_plus})("value")
+    g.output({"loop_acc": a_plus, "should_continue": pred})
     return g
 
 
 def simple_loop() -> GraphData:
     g = GraphData()
-    six = g.add(Const(6))(Labels.VALUE)
-    g_const = g.add(Const(loop_body()))(Labels.VALUE)
-    loop = g.add(Loop(g_const, {"loop_acc": six}, "should_continue"))
-    g.add(Output({"simple_loop_output": loop("loop_acc")}))
+    six = g.const(6)
+    g_const = g.const(loop_body())
+    loop = g.loop(g_const, {"loop_acc": six}, "should_continue")
+    g.output({"simple_loop_output": loop("loop_acc")})
     return g
 
 
 def simple_map() -> GraphData:
     g = GraphData()
-    six = g.add(Const(6))(Labels.VALUE)
-    Ns_const = g.add(Const(list(range(21))))(Labels.VALUE)
-    Ns = g.add(Func("builtins.unfold_values", {Labels.VALUE: Ns_const}))
-    doubler_const = g.add(Const(doubler_plus()))(Labels.VALUE)
+    six = g.const(6)
+    Ns_const = g.const(list(range(21)))
+    Ns = g.func("builtins.unfold_values", {Labels.VALUE: Ns_const})
+    doubler_const = g.const(doubler_plus())
 
-    map_def = Map(doubler_const, {"doubler_input": Ns("*"), "intercept": six})
-    m = g.add(map_def)
-    folded = g.add(Func("builtins.fold_values", {"values_glob": m("*")}))
-    g.add(Output({"simple_map_output": folded(Labels.VALUE)}))
+    m = g.map(doubler_const, {"doubler_input": Ns("*"), "intercept": six})
+    folded = g.func("builtins.fold_values", {"values_glob": m("*")})
+    g.output({"simple_map_output": folded(Labels.VALUE)})
     return g
 
 
 def simple_ifelse() -> GraphData:
     g = GraphData()
-    pred = g.add(Input("pred"))("pred")
-    one = g.add(Const(1))(Labels.VALUE)
-    two = g.add(Const(2))(Labels.VALUE)
-    out = g.add(IfElse(pred, one, two))(Labels.VALUE)
-    g.add(Output({"simple_ifelse_output": out}))
+    pred = g.input("pred")
+    one = g.const(1)
+    two = g.const(2)
+    out = g.if_else(pred, one, two)("value")
+    g.output({"simple_ifelse_output": out})
     return g
 
 
 def maps_in_series() -> GraphData:
     g = GraphData()
-    zero = g.add(Const(0))(Labels.VALUE)
-    Ns_const = g.add(Const(list(range(21))))(Labels.VALUE)
-    Ns = g.add(Func("builtins.unfold_values", {Labels.VALUE: Ns_const}))
-    doubler_const = g.add(Const(doubler_plus()))(Labels.VALUE)
+    zero = g.const(0)
 
-    map_def = Map(doubler_const, {"doubler_input": Ns("*"), "intercept": zero})
-    m = g.add(map_def)
+    Ns_const = g.const(list(range(21)))
+    Ns = g.func("builtins.unfold_values", {Labels.VALUE: Ns_const})
+    doubler_const = g.const(doubler_plus())
 
-    map_def2 = Map(doubler_const, {"doubler_input": m("*"), "intercept": zero})
-    m2 = g.add(map_def2)
-    folded = g.add(Func("builtins.fold_values", {"values_glob": m2("*")}))
-    g.add(Output({"maps_in_series_output": folded(Labels.VALUE)}))
+    m = g.map(doubler_const, {"doubler_input": Ns("*"), "intercept": zero})
+
+    m2 = g.map(doubler_const, {"doubler_input": m("*"), "intercept": zero})
+    folded = g.func("builtins.fold_values", {"values_glob": m2("*")})
+    g.output({"maps_in_series_output": folded(Labels.VALUE)})
     return g
 
 
 def map_with_str_keys() -> GraphData:
     g = GraphData()
-    zero = g.add(Const(0))(Labels.VALUE)
-    Ns_const = g.add(Const({"one": 1, "two": 2, "three": 3}))(Labels.VALUE)
-    Ns = g.add(Func("builtins.unfold_dict", {Labels.VALUE: Ns_const}))
-    doubler_const = g.add(Const(doubler_plus()))(Labels.VALUE)
+    zero = g.const(0)
+    Ns_const = g.const({"one": 1, "two": 2, "three": 3})
+    Ns = g.func("builtins.unfold_dict", {Labels.VALUE: Ns_const})
+    doubler_const = g.const(doubler_plus())
 
-    map_def = Map(doubler_const, {"doubler_input": Ns("*"), "intercept": zero})
-    m = g.add(map_def)
-    folded = g.add(Func("builtins.fold_dict", {"values_glob": m("*")}))
-    g.add(Output({"map_with_str_keys_output": folded(Labels.VALUE)}))
+    m = g.map(doubler_const, {"doubler_input": Ns("*"), "intercept": zero})
+    folded = g.func("builtins.fold_dict", {"values_glob": m("*")})
+    g.output({"map_with_str_keys_output": folded(Labels.VALUE)})
     return g
 
 
 def factorial() -> GraphData:
     g = GraphData()
-    minus_one = g.add(Const(-1))(Labels.VALUE)
-    one = g.add(Const(1))(Labels.VALUE)
-    n = g.add(Input("n"))("n")
-    fac = g.add(Input("factorial"))("factorial")
+    minus_one = g.const(-1)
+    one = g.const(1)
+    n = g.input("n")
+    fac = g.input("factorial")
 
-    pred = g.add(Func("builtins.igt", {"a": n, "b": one}))(Labels.VALUE)
+    pred = g.func("builtins.igt", {"a": n, "b": one})("value")
 
-    n_minus_one = g.add(Func("builtins.iadd", {"a": minus_one, "b": n}))(Labels.VALUE)
-    rec = g.add(Eval(fac, {"n": n_minus_one, "factorial": fac}))("factorial_output")
-    if_true = g.add(Func("builtins.itimes", {"a": n, "b": rec}))(Labels.VALUE)
+    n_minus_one = g.func("builtins.iadd", {"a": minus_one, "b": n})("value")
+    rec = g.eval(fac, {"n": n_minus_one, "factorial": fac})("factorial_output")
+    if_true = g.func("builtins.itimes", {"a": n, "b": rec})("value")
 
-    if_false = g.add(Const(1))(Labels.VALUE)
+    if_false = g.const(1)
 
-    out = g.add(IfElse(pred, if_true, if_false))(Labels.VALUE)
-    g.add(Output({"factorial_output": out}))
+    out = g.if_else(pred, if_true, if_false)("value")
+    g.output({"factorial_output": out})
     return g
 
 
 def simple_eagerifelse() -> GraphData:
     g = GraphData()
-    pred = g.add(Input("pred"))("pred")
-    one = g.add(Const(1))(Labels.VALUE)
-    two = g.add(Const(2))(Labels.VALUE)
-    out = g.add(EagerIfElse(pred, one, two))(Labels.VALUE)
-    g.add(Output({"simple_eagerifelse_output": out}))
+    pred = g.input("pred")
+    one = g.const(1)
+    two = g.const(2)
+    out = g.eager_if_else(pred, one, two)(Labels.VALUE)
+    g.output({"simple_eagerifelse_output": out})
     return g

c22cd90d0250630b18fbd0d946f01202c23bb842
Matthew Burke <matthew.burke@quantinuum.com>
2025-09-08 10:54:43 +0100

    Add WithCallArgs

--- tierkreis/tierkreis/runner/uv_run.py
+++ tierkreis/tierkreis/runner/uv_run.py
@@ -1,9 +1,4 @@
-class UvRun:
-    def __init__(self, uv_project_directory: str) -> None:
-        self.project_dir = uv_project_directory
-
-    def __call__(self, inner_command: str) -> str:
-        return f"uv run --project {self.project_dir} {inner_command}"
+from typing import Any
 
 
 class StdOutIn:
@@ -13,6 +8,22 @@ class StdOutIn:
 
     def __call__(self, inner_command: str) -> str:
         return f"{inner_command} < {self.input} > {self.output}"
+
+
+class WithCallArgs:
+    def __init__(self, call_args_path: str) -> None:
+        self.call_args_path = call_args_path
+
+    def __call__(self, inner_command: str) -> Any:
+        return f"{inner_command} {self.call_args_path}"
+
+
+class UvRun:
+    def __init__(self, uv_project_directory: str) -> None:
+        self.project_dir = uv_project_directory
+
+    def __call__(self, inner_command: str) -> str:
+        return f"uv run --project {self.project_dir} {inner_command}"
 
 
 class DockerRun:

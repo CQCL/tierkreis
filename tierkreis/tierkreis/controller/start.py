65c50a52509ae9f92c09364b72573ac9b8a4436a
Matthew Burke <matthew.burke@quantinuum.com>
2025-09-09 15:46:11 +0100

    Satisfy ruff

--- tierkreis/tierkreis/controller/start.py
+++ tierkreis/tierkreis/controller/start.py
@@ -8,7 +8,6 @@ from tierkreis.controller.data.types import bytes_from_ptype, ptype_from_bytes
 from tierkreis.controller.executor.in_memory_executor import InMemoryExecutor
 from tierkreis.controller.storage.adjacency import outputs_iter
 from tierkreis.paths import Paths
-from tierkreis.runner.commands import HandleError
 from typing_extensions import assert_never
 
 from tierkreis.consts import PACKAGE_PATH

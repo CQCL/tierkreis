3b24aa6b956e7dbf8154d772b24969b80ff56572
Matthew Burke <matthew.burke@quantinuum.com>
2025-09-26 10:38:29 +0100

    Remove underscore

--- tierkreis/tierkreis/controller/storage/pathstorage.py
+++ tierkreis/tierkreis/controller/storage/pathstorage.py
@@ -20,14 +20,14 @@ class PathStorageBackend(Protocol):
     workflow_id: UUID
     name: str
 
-    def _delete(self) -> None: ...
-    def _exists(self, path: Path) -> bool: ...
-    def _list_output_paths(self, output_dir: Path) -> list[Path]: ...
-    def _link(self, src: Path, dst: Path) -> None: ...
-    def _read(self, path: Path) -> bytes: ...
-    def _stat(self, path: Path) -> StatResult: ...
-    def _touch(self, path: Path, is_dir: bool = False) -> None: ...
-    def _write(self, path: Path, value: bytes) -> None: ...
+    def delete(self) -> None: ...
+    def exists(self, path: Path) -> bool: ...
+    def list_output_paths(self, output_dir: Path) -> list[Path]: ...
+    def link(self, src: Path, dst: Path) -> None: ...
+    def read(self, path: Path) -> bytes: ...
+    def stat(self, path: Path) -> StatResult: ...
+    def touch(self, path: Path, is_dir: bool = False) -> None: ...
+    def write(self, path: Path, value: bytes) -> None: ...
 
 
 class PathStorageBase(PathStorageBackend):
@@ -64,14 +64,14 @@ class PathStorageBase(PathStorageBackend):
         return self.workflow_dir / str(node_location) / "errors"
 
     def clean_graph_files(self) -> None:
-        self._delete()
+        self.delete()
 
     def write_node_def(self, node_location: Loc, node: NodeDef):
         bs = NodeDefModel(root=node).model_dump_json().encode()
-        self._write(self._nodedef_path(node_location), bs)
+        self.write(self._nodedef_path(node_location), bs)
 
     def read_node_def(self, node_location: Loc) -> NodeDef:
-        bs = self._read(self._nodedef_path(node_location))
+        bs = self.read(self._nodedef_path(node_location))
         return NodeDefModel(**json.loads(bs)).root
 
     def write_worker_call_args(
@@ -97,17 +97,17 @@ class PathStorageBase(PathStorageBackend):
             error_path=self._error_path(node_location).relative_to(self.tkr_dir),
             logs_path=self.logs_path.relative_to(self.tkr_dir),
         )
-        self._write(call_args_path, node_definition.model_dump_json().encode())
-        self._touch(self._outputs_dir(node_location), is_dir=True)
+        self.write(call_args_path, node_definition.model_dump_json().encode())
+        self.touch(self._outputs_dir(node_location), is_dir=True)
 
         if (parent := node_location.parent()) is not None:
-            self._touch(self._metadata_path(parent))
+            self.touch(self._metadata_path(parent))
 
         return call_args_path.relative_to(self.tkr_dir)
 
     def read_worker_call_args(self, node_location: Loc) -> WorkerCallArgs:
         node_definition_path = self._worker_call_args_path(node_location)
-        return WorkerCallArgs(**json.loads(self._read(node_definition_path)))
+        return WorkerCallArgs(**json.loads(self.read(node_definition_path)))
 
     def link_outputs(
         self,
@@ -117,9 +117,9 @@ class PathStorageBase(PathStorageBackend):
         old_port: PortID,
     ) -> None:
         new_dir = self._output_path(new_location, new_port)
-        self._touch(self._outputs_dir(new_location), is_dir=True)
+        self.touch(self._outputs_dir(new_location), is_dir=True)
         try:
-            self._link(self._output_path(old_location, old_port), new_dir)
+            self.link(self._output_path(old_location, old_port), new_dir)
         except FileNotFoundError as e:
             raise TierkreisError(
                 f"Could not link {e.filename} to {e.filename2}."
@@ -134,63 +134,63 @@ class PathStorageBase(PathStorageBackend):
         self, node_location: Loc, output_name: PortID, value: bytes
     ) -> Path:
         output_path = self._output_path(node_location, output_name)
-        self._write(output_path, bytes(value))
+        self.write(output_path, bytes(value))
         return output_path
 
     def read_output(self, node_location: Loc, output_name: PortID) -> bytes:
-        return self._read(self._output_path(node_location, output_name))
+        return self.read(self._output_path(node_location, output_name))
 
     def read_errors(self, node_location: Loc) -> str:
-        if not self._exists(self._error_logs_path(node_location)):
-            if self._exists(self._error_path(node_location)):
-                return self._read(self._error_path(node_location)).decode()
+        if not self.exists(self._error_logs_path(node_location)):
+            if self.exists(self._error_path(node_location)):
+                return self.read(self._error_path(node_location)).decode()
             return ""
-        errors = self._read(self._error_logs_path(node_location)).decode()
+        errors = self.read(self._error_logs_path(node_location)).decode()
         if errors == "":
-            if self._exists(self._error_path(node_location)):
-                return self._read(self._error_path(node_location)).decode()
+            if self.exists(self._error_path(node_location)):
+                return self.read(self._error_path(node_location)).decode()
         return errors
 
     def write_node_errors(self, node_location: Loc, error_logs: str) -> None:
-        self._write(self._error_logs_path(node_location), error_logs.encode())
+        self.write(self._error_logs_path(node_location), error_logs.encode())
 
     def read_output_ports(self, node_location: Loc) -> list[PortID]:
-        dir_list = self._list_output_paths(self._outputs_dir(node_location))
+        dir_list = self.list_output_paths(self._outputs_dir(node_location))
         dir_list.sort()
         return [x.name for x in dir_list]
 
     def is_node_started(self, node_location: Loc) -> bool:
-        return self._exists(Path(self._nodedef_path(node_location)))
+        return self.exists(Path(self._nodedef_path(node_location)))
 
     def is_node_finished(self, node_location: Loc) -> bool:
-        return self._exists(self._done_path(node_location))
+        return self.exists(self._done_path(node_location))
 
     def node_has_error(self, node_location: Loc) -> bool:
-        return self._exists(self._error_path(node_location))
+        return self.exists(self._error_path(node_location))
 
     def mark_node_finished(self, node_location: Loc) -> None:
-        self._touch(self._done_path(node_location))
+        self.touch(self._done_path(node_location))
 
         if (parent := node_location.parent()) is not None:
-            self._touch(self._metadata_path(parent))
+            self.touch(self._metadata_path(parent))
 
     def write_metadata(self, node_location: Loc) -> None:
         j = json.dumps({"name": self.name, "start_time": datetime.now().isoformat()})
-        self._write(self._metadata_path(node_location), j.encode())
+        self.write(self._metadata_path(node_location), j.encode())
 
     def read_metadata(self, node_location: Loc) -> dict[str, Any]:
-        return json.loads(self._read(self._metadata_path(node_location)))
+        return json.loads(self.read(self._metadata_path(node_location)))
 
     def read_started_time(self, node_location: Loc) -> str | None:
         node_def = Path(self._nodedef_path(node_location))
-        if not self._exists(node_def):
+        if not self.exists(node_def):
             return None
-        since_epoch = self._stat(node_def).st_mtime
+        since_epoch = self.stat(node_def).st_mtime
         return datetime.fromtimestamp(since_epoch).isoformat()
 
     def read_finished_time(self, node_location: Loc) -> str | None:
         done = Path(self._done_path(node_location))
-        if not self._exists(done):
+        if not self.exists(done):
             return None
-        since_epoch = self._stat(done).st_mtime
+        since_epoch = self.stat(done).st_mtime
         return datetime.fromtimestamp(since_epoch).isoformat()

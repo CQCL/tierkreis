009a09e9a976d3cbe65c9072c4b3a190aeb37554
Matthew Burke <matthew.burke@quantinuum.com>
2025-11-20 16:09:36 +0000

    Update test

--- examples/example_workers/scipy_worker/main.py
+++ examples/example_workers/scipy_worker/main.py
@@ -1,19 +1,53 @@
+import io
+import os
+import pickle
 from sys import argv
-from typing import NamedTuple
+from typing import Annotated, NamedTuple
 import numpy as np
 
+from tierkreis.controller.data.core import Deserializer, Serializer
 from tierkreis.worker.worker import Worker
 
 worker = Worker("scipy_worker")
 
 
+def save(a: np.ndarray) -> bytes:
+    with io.BytesIO() as bs:
+        np.save(bs, a)
+        return bs.getvalue()
+
+
+def load(bs: bytes) -> np.ndarray:
+    with io.BytesIO() as bi:
+        bi.write(bs)
+        bi.seek(0)
+        return np.load(bi, encoding="bytes")
+
+
+SER_METHOD = os.environ.get("SER_METHOD")
+if SER_METHOD == "dumps":
+    ser = Serializer(np.ndarray.dumps)
+    deser = Deserializer(pickle.loads)
+elif SER_METHOD == "tolist":
+    ser = Serializer(np.ndarray.tolist)
+    deser = Deserializer(np.array)
+elif SER_METHOD == "save":
+    ser = Serializer(save)
+    deser = Deserializer(load)
+else:
+    ser = None
+    deser = None
+
+NDArray = Annotated[np.ndarray, ser, deser]
+
+
 class PointedArray(NamedTuple):
-    a: np.ndarray
+    a: NDArray
     p: int
 
 
 @worker.task()
-def add_point(a: np.ndarray, p: int) -> PointedArray:
+def add_point(a: NDArray, p: int) -> PointedArray:
     return PointedArray(a, p)
 
 
@@ -23,17 +57,17 @@ def eval_point(pa: PointedArray) -> float:
 
 
 @worker.task()
-def linspace(start: float, stop: float, num: int = 50) -> np.ndarray:
+def linspace(start: float, stop: float, num: int = 50) -> NDArray:
     return np.linspace(start, stop, num=num)
 
 
 @worker.task()
-def transpose(a: np.ndarray) -> np.ndarray:
+def transpose(a: NDArray) -> NDArray:
     return a.transpose()
 
 
 @worker.task()
-def reshape(a: np.ndarray, shape: int | list[int]) -> np.ndarray:
+def reshape(a: NDArray, shape: int | list[int]) -> NDArray:
     return np.reshape(a, shape)
 
 

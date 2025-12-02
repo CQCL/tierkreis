f1d367aa1ca12f1edab0568aa3026da79ca1e856
Matthew Burke <matthew.burke@quantinuum.com>
2025-12-02 13:58:22 +0000

    Handle multiple EVALs

--- tierkreis_visualization/frontend/src/nodes/eval.tsx
+++ tierkreis_visualization/frontend/src/nodes/eval.tsx
@@ -5,123 +5,13 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Edge, type NodeProps, useReactFlow } from "@xyflow/react";
+import { type NodeProps } from "@xyflow/react";
 
 import { InputHandleArray, OutputHandleArray } from "@/components/handles";
 import { NodeStatusIndicator } from "@/components/StatusIndicator";
-import { Button } from "@/components/ui/button";
-import { parseEdges, parseNodes } from "@/graph/parseGraph";
-import { bottomUpLayout } from "@/graph/layoutGraph";
 import { type BackendNode } from "./types";
-import { hideChildren } from "./hide_children";
 import { Minus, Plus } from "lucide-react";
-import { fetchNode } from "@/data/api";
 import { Link, useNavigate } from "@tanstack/react-router";
-
-function replaceEval(
-  nodeId: string,
-  newNodes: BackendNode[],
-  newEdges: Edge[],
-  oldNodes: BackendNode[],
-  oldEdges: Edge[]
-) {
-  // replaces an eval node with its nested subgraph
-  const oldEdgesCopy: Edge[] = JSON.parse(JSON.stringify(oldEdges));
-  // we only care about the last part of the id as number
-  newNodes.sort(
-    (a, b) =>
-      Number(a.id.substring(a.id.lastIndexOf(":"), a.id.length)) -
-      Number(b.id.substring(b.id.lastIndexOf(":"), b.id.length))
-  );
-  oldEdgesCopy.forEach((edge) => {
-    if (edge.target == nodeId && edge.label != "Graph Body") {
-      // find the correct node which has an output handle of the form id:\dport_name
-      let found = false;
-      for (const node of newNodes) {
-        if (node.id.startsWith(nodeId)) {
-          node.data.handles.outputs.forEach((value) => {
-            if (edge.targetHandle?.endsWith(value)) {
-              node.data.handles.inputs.push(value);
-              edge.targetHandle = node.id + "_" + value;
-              edge.target = node.id;
-              found = true;
-            }
-          });
-          if (found) {
-            break;
-          }
-        }
-      }
-      if (!found) {
-        // workaround for elements inside map, only works correctly if the unfolded value is mapped to the first input
-        const node = newNodes[0];
-        const value = edge.targetHandle?.split("_")[1] || "";
-        node.data.handles.inputs.push(value);
-        edge.targetHandle = node.id + "_" + value;
-        edge.target = node.id;
-      }
-    }
-    if (edge.source == nodeId) {
-      console.log(edge.label);
-      let found = false;
-      for (let index = newNodes.length - 1; index >= 0; index--) {
-        const node = newNodes[index];
-        if (node.id.startsWith(nodeId)) {
-          node.data.handles.inputs.forEach((value) => {
-            if (edge.sourceHandle?.endsWith(value) && value != "loop") {
-              // != "loop" is a hack to fix loops with a single iteration TODO: check if this really works
-              node.data.handles.outputs.push(value);
-              edge.sourceHandle = node.id + "_" + value;
-              edge.source = node.id;
-              found = true;
-            }
-          });
-          if (found) {
-            break;
-          }
-        }
-      }
-      if (!found) {
-        // workaround for elements inside map, only works correctly if there is a single output
-        const node = newNodes[newNodes.length - 1];
-        const value = edge.sourceHandle?.split("_")[1] || "";
-        node.data.handles.outputs.push(value);
-        edge.sourceHandle = node.id + "_" + value;
-        edge.source = node.id;
-      }
-    }
-  });
-  // update the internal state of the eval node
-  oldNodes = oldNodes
-    .map((node) => {
-      if (node.id === nodeId) {
-        node.position = { x: 0, y: 0 };
-        node.data.hidden_handles = {
-          inputs: node.data.handles.inputs,
-          outputs: node.data.handles.outputs,
-        };
-        node.data.hidden_edges = oldEdges.filter(
-          (edge) =>
-            (edge.target === nodeId || edge.source === nodeId) &&
-            edge.label !== "Graph Body"
-        );
-        console.log(node.data.hidden_edges);
-        node.data.handles = { inputs: ["body"], outputs: [] };
-        node.data.is_expanded = true;
-      }
-      return node;
-    })
-    .filter((node): node is BackendNode => node !== undefined);
-  const tmpEdges = oldEdgesCopy.filter(
-    (edge) =>
-      (edge.target !== nodeId && edge.source !== nodeId) ||
-      edge.label === "Graph Body"
-  );
-  return {
-    nodes: [...oldNodes, ...newNodes],
-    edges: [...tmpEdges, ...newEdges],
-  };
-}
 
 export function EvalNode({ data: node_data }: NodeProps<BackendNode>) {
   const navigate = useNavigate();
@@ -130,6 +20,34 @@ export function EvalNode({ data: node_data }: NodeProps<BackendNode>) {
       to: "/workflows/$wid/nodes/$loc",
       params: { wid: node_data.workflowId, loc: node_data.node_location },
     });
+  };
+
+  const openLink = () => {
+    const params = { wid: node_data.workflowId, loc: "-" };
+    const search = (prev: { openEvals?: string[] | undefined }) => {
+      const openEvals = [node_data.node_location, ...(prev.openEvals ?? [])];
+      return { openEvals };
+    };
+    return (
+      <Link to="/workflows/$wid/nodes/$loc" params={params} search={search}>
+        <Plus />
+      </Link>
+    );
+  };
+
+  const closeLink = () => {
+    const params = { wid: node_data.workflowId, loc: "-" };
+    const search = (prev: { openEvals?: string[] | undefined }) => {
+      const openEvals = prev.openEvals?.filter(
+        (x) => !x.startsWith(node_data.node_location)
+      );
+      return { openEvals };
+    };
+    return (
+      <Link to="/workflows/$wid/nodes/$loc" params={params} search={search}>
+        <Minus style={{ width: 48, height: 48 }} />
+      </Link>
+    );
   };
 
   if (node_data.is_expanded) {
@@ -142,18 +60,7 @@ export function EvalNode({ data: node_data }: NodeProps<BackendNode>) {
           hoveredId={node_data.hoveredId}
           setHoveredId={node_data.setHoveredId}
         />
-        <div className="grid justify-items-end">
-          <Link
-            to="/workflows/$wid/nodes/$loc"
-            params={{
-              wid: node_data.workflowId,
-              loc: "-",
-            }}
-            search={{ openEvals: [] }}
-          >
-            <Minus style={{ width: 48, height: 48 }} />
-          </Link>
-        </div>
+        <div className="grid justify-items-end">{closeLink()}</div>
         <OutputHandleArray
           handles={node_data.handles.outputs}
           id={node_data.id}
@@ -175,18 +82,7 @@ export function EvalNode({ data: node_data }: NodeProps<BackendNode>) {
         </CardHeader>
         <CardContent>
           <div className="flex items-center justify-center">
-            {node_data.status != "Not started" && (
-              <Link
-                to="/workflows/$wid/nodes/$loc"
-                params={{
-                  wid: node_data.workflowId,
-                  loc: "-",
-                }}
-                search={{ openEvals: [node_data.node_location] }}
-              >
-                <Plus />
-              </Link>
-            )}
+            {node_data.status != "Not started" && openLink()}
           </div>
           <InputHandleArray
             handles={node_data.handles.inputs}
